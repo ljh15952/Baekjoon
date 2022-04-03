2d34e5b25bf297762b21d5f6a52af5e2663fb5c3
중화이 <ljh15952@gmail.com>
2022-04-03 06:10:56 +0000

    4/3 15:10

---
+++ src/2. Conditional /2480.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int d1,d2,d3;
+	int money = 0;
+	cin >> d1 >> d2 >> d3;
+	
+	if(d1 == d2 && d2 == d3){
+		money = 10000 + (d1 * 1000);
+	}else if(d1 == d2){
+		money = 1000 + (d1 * 100);
+	}else if(d2 == d3){
+		money = 1000 + (d2 * 100);
+	}else if(d1 == d3){
+		money = 1000 + (d3 * 100);
+	}else{
+		int max = (d1 >= d2 && d1 >= d3) ? d1 :
+		(d2 >= d1 && d2 >= d3) ? d2 : d3;
+		money = 1000 + (max * 100);
+	}
+	
+	cout << money << endl;
+	return 0;
+}
