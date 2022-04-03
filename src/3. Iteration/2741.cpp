e0131b8d443e67cc4bd2693354ca98b4b95b7d59
중화이 <ljh15952@gmail.com>
2022-04-03 06:45:48 +0000

    4/3 15:45

---
+++ src/3. Iteration/2741.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	cin.tie(NULL);
+	int n;
+	
+	cin >> n;
+	
+	
+	for(int i = 1; i <= n; i++){
+		cout << i;
+		printf("\n");
+	}
+	return 0;
+}
---
+++ src/3. Iteration/2742.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	cin.tie(NULL);
+	int n;
+	
+	cin >> n;
+	
+	
+	for(int i = n; i >= 1; i--){
+		cout << i;
+		printf("\n");
+	}
+	return 0;
+}
