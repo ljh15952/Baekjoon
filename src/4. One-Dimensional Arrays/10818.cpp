413a510157f0af3ab9ba7d69cd5b41003d4d041b
중화이 <ljh15952@gmail.com>
2022-04-04 11:42:57 +0000

    4/4 8:42 Code fix

--- src/4. One-Dimensional Arrays/10818.cpp
+++ src/4. One-Dimensional Arrays/10818.cpp
@@ -3,25 +3,47 @@
 using namespace std;
 
 int main(){
-	cin.tie(NULL);
 	int n;
-	
 	cin >> n;
 	
-	int min = 9999999;
-	int max = -9999999;
+	int arr[1000000];
+	
 	for(int i = 0; i < n; i++){
-		int num;
-		
-		cin >> num;
-		
-		if(num > max)
-			max = num;
-		if(num < min)
-			min = num;
+		scanf("%d",&arr[i]);
+	}
+	
+	int max = arr[0];
+	int min = arr[0];
+	for(int i = 1; i < n; i++){
+		if(arr[i] > max){
+			max = arr[i];
+		}
+		if(arr[i] < min){
+			min = arr[i];
+		}
 	}
 	
 	cout << min << " " << max << endl;
+	
+	// cin.tie(NULL);
+	// int n;
+	
+	// cin >> n;
+	
+	// int min = 9999999;
+	// int max = -9999999;
+	// for(int i = 0; i < n; i++){
+	// 	int num;
+		
+	// 	cin >> num;
+		
+	// 	if(num > max)
+	// 		max = num;
+	// 	if(num < min)
+	// 		min = num;
+	// }
+	
+	// cout << min << " " << max << endl;
 	
 	return 0;
 }
--- src/4. One-Dimensional Arrays/2562.cpp
+++ src/4. One-Dimensional Arrays/2562.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main(){
+	int arr[9];
 	
-	int max = -99999;
+	for(int i = 0; i < 9; i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	int max = arr[0];
 	int num = 0;
-	for(int i = 0; i < 9; i++)
-	{
-		int n;
-		cin >> n;
-		if(n > max){
-			max = n;
+	
+	for(int i = 1; i < 9; i++){
+		if(arr[i] > max){
+			max = arr[i];
 			num = i+1;
 		}
 	}
@@ -19,7 +22,21 @@ int main(){
 	cout << max << endl;
 	cout << num << endl;
 	
+	//배열을 안씀
+	// int max = -99999;
+	// int num = 0;
+	// for(int i = 0; i < 9; i++)
+	// {
+	// 	int n;
+	// 	cin >> n;
+	// 	if(n > max){
+	// 		max = n;
+	// 		num = i+1;
+	// 	}
+	// }
 	
-	
+	// cout << max << endl;
+	// cout << num << endl;
+
 	return 0;
 }
