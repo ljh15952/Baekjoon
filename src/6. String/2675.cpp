d95fff2fcae36ece3b50137f7c1b62be6c66d241
중화이 <ljh15952@gmail.com>
2022-04-09 07:39:10 +0000

    4/9 16:39

---
+++ src/6. String/2675.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin >> n;
+	
+	
+	for(int i = 0; i < n; ++i){
+		int size;
+		cin >> size;
+		char str[20];
+		scanf("%s",str);
+
+		string s;
+		int index = 0;
+		while(str[index] != '\0'){
+			for(int k = 0; k < size; ++k){
+				s += str[index];
+			}
+			index++;
+		}
+
+		cout << s << endl;
+	}
+	return 0;
+}
