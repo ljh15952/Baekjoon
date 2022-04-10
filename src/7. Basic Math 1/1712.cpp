886ef2ae329b933dce187b2690242fb626e96662
중화이 <ljh15952@gmail.com>
2022-04-10 09:57:56 +0000

    4/10 18:57

---
+++ src/7. Basic Math 1/1712.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a,b,c;
+	cin >> a >> b >> c;
+	
+	int sum = (b > c) ? -1 : (c - b ==0) ? -1 : (a / (c - b) + 1);
+	
+
+	return 0;
+}
