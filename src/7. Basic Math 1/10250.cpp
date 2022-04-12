cd6b21ee149d33cfdc805cc6c2c199ec28a98bdc
중화이 <ljh15952@gmail.com>
2022-04-12 10:23:48 +0000

    4/12 19:23

---
+++ src/7. Basic Math 1/10250.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int t;
+	
+	cin >> t;
+	
+	//6 12 10 -> 402
+	//층 방수 몇번째 손님
+	for(int i = 0; i < t; i++){
+		int H,W,N;
+
+		cin >> H >> W >> N;
+		if(N % H == 0){
+			int first = H;
+			int last = N / H;
+			printf("%d%02d",first,last);
+		}
+		else{
+			int first = N % H;
+			int last = N / H + 1;
+
+			printf("%d%02d",first,last);
+		}
+	}
+	
+	
+	// //6 12 10 -> 402
+	// //층 방수 몇번째 손님
+	// 6 12 12
+
+	
+		
+	return 0;
+}
