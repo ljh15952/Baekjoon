bef9b87a4b75c066962f94017c5274a029c7bed5
중화이 <ljh15952@gmail.com>
2022-04-16 07:13:25 +0000

    4/16 16:13

---
+++ src/8. Basic Math 2/11653.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	
+	int n;
+	
+	cin >> n;
+	
+	vector<int> arr;
+	
+	
+	
+	int k = 2;
+	
+	while(n > 1){
+		if(n % k == 0){
+			arr.push_back(k);
+			n /= k;
+		}else{
+			k++;
+		}
+	}
+	
+	for(int i : arr){
+		cout << i << endl;
+	}
+	
+	return 0;
+}
