a000e35e163e8a0ca02ddfc30b6091360b7c988f
중화이 <ljh15952@gmail.com>
2022-04-17 10:07:39 +0000

    4/17 19:07

--- src/8. Basic Math 2/9020.cpp
+++ src/8. Basic Math 2/9020.cpp
@@ -4,57 +4,36 @@
 using namespace std;
 
 int main(){
-	
-	// int T;
-	
-	// scanf("%d",&T);
-	
 	int arr[10001] = {0,};
 	arr[1] = 1;
 	
-	vector<int> sosu;
-		
 	for(int i = 2; i <= 10000; ++i){
 		for(int j = 2; j * i <= 10000; ++j){
 			arr[i*j] = 1;
 		}
 	}
 	
-	for(int i = 2; i <= 100; ++i){
-		if(arr[i] == 0){
-		 	sosu.push_back(i);
-		}
-	}
+	int T;
+	scanf("%d",&T);
 	
-	int n;
-	
-	cin >> n;
-	
-	int sum = 0;
-	
-	int min = 999999;
-	int a,b;	
-	
-	//TODO
-	/*
-	만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
-	*/
-	
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			if(n == sosu[i]+sosu[j]){
-				//cout << sosu[i] << "+" << sosu[j] << "= " << n << endl;
-				//cout << sosu[i] << " " << sosu[j] << endl;
-				if(min > abs(sosu[i] - sosu[j])){
-					min = sosu[i] - sosu[j];
-					a = sosu[i];
-					b = sosu[j];
-				}
+	for(int x = 0; x < T; x++){
+		int n;
+
+		cin >> n;
+
+		int pivot = n / 2;
+		
+		for(int i = 0; i < pivot; ++i){
+			int num1 = pivot - i;
+			int num2 = n - num1;
+			
+			if(arr[num1] == 0 && arr[num2] == 0){
+				cout << num1 << ' ' << num2 << endl;
+				break;
 			}
 		}
 	}
 	
-	cout << a << " " << b << endl;
 	
 	return 0;
 }
