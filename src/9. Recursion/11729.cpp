b65865c9909cacf9d32877dfa8094eb6ea36e0ee
중화이 <ljh15952@gmail.com>
2022-04-20 10:14:40 +0000

    4/20 19:14

--- src/9. Recursion/11729.cpp
+++ src/9. Recursion/11729.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+void hanoi(int n, int start, int mid, int to){
+	if(n == 1){
+		cout << start << ' ' << to << endl;
+		return;
+	}
+	
+	hanoi(n - 1, start, to, mid);
+	
+	cout << start << '1' << to << endl;
+	
+	hanoi(n - 1, mid, start, to);
+	
+}
+
 int main(){
 	
-	cout << "HI im towers of Hanoi" << endl;
+	int n;
+	
+	cin >> n;
+	cout << (1 << n) - 1 << endl;
+	hanoi(n,1,2,3);
 	
 	return 0;
 }
