dabfb871d4909b780e3ca47a121be9c901524e9f
중화이 <ljh15952@gmail.com>
2022-04-23 10:20:33 +0000

    4/23 19:20

---
+++ src/13. NT and combinatorics/5086.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a,b;
+	cin >> a >> b;
+	
+	while(a != 0 && b != 0){
+		
+		if(b % a == 0){
+			cout << "factor" << endl;
+		}else if(a % b == 0){
+			cout << "multiple" << endl;
+		}else{
+			cout << "neither" << endl;
+		}
+		
+		cin >> a >> b;
+	}
+	
+	return 0;
+}
