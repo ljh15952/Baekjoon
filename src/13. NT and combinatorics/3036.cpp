173fc73931eb69b170fa21bfb5222dc5585ecc6f
중화이 <ljh15952@gmail.com>
2022-04-25 10:01:25 +0000

    4/25 19:01

--- src/13. NT and combinatorics/3036.cpp
+++ src/13. NT and combinatorics/3036.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+int gcd(int a, int b){
+	if(b == 0){
+		return a;
+	}else{
+		return gcd(b,a%b);
+	}
+}
 
 int main(){
 
-	cout << "Start" << endl;
-
+	int n;
+	cin >> n;
+	
+	vector<int> arr;
+	for(int i = 0; i < n; i++){
+		int num;
+		scanf("%d",&num);
+		arr.push_back(num);
+	}
+	
+	for(int i = 1; i < n; i++){
+		int gcdNum = gcd(arr[0],arr[i]);
+		cout << arr[0] / gcdNum << "/" << arr[i] / gcdNum << '\n';
+	}
+	
 	return 0;
 }
