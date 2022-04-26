a2470078db9943e6628bdafbd75d3244c26cfe69
중화이 <ljh15952@gmail.com>
2022-04-26 11:06:39 +0000

    4/26 20:06

--- src/13. NT and combinatorics/1010.cpp
+++ src/13. NT and combinatorics/1010.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
-int main(){
+int dp[30][30];
 
-	cout << "HIHI" << endl;
+int main(){
+	
+	//조합 공식 이용
+	//혼자 못풀겠음ㅠ
+	
+	for(int i = 0; i < 30; ++i){
+		dp[i][i] = 1;
+		dp[i][0] = 1;
+	}
+	
+	for(int i = 2; i < 30; ++i){
+		for(int j = 1; j < 30; ++j){
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+		}
+	}
+	
+	int n,m,t;
+	
+	cin >> t;
+	
+	for(int i = 0; i < t; ++i){
+		cin >> n >> m;
+		
+		cout << dp[m][n] << endl;
+	}
+	
 
 	return 0;
 }
