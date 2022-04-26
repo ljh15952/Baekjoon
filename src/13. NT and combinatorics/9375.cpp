76b34cd717fa6a4ffe06a7565e37fe402dd53ab8
중화이 <ljh15952@gmail.com>
2022-04-26 11:20:34 +0000

    4/26 20:20

--- src/13. NT and combinatorics/9375.cpp
+++ src/13. NT and combinatorics/9375.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
-
+#include <map>
+#include <string>
 using namespace std;
 
 int main(){
 	
-	cout << "HI?" << endl;
+	int t;
+	
+	cin >> t;
+	
+	/* 
+	공식 
+	headgear 2
+	eyewear 1
+	-> (2+1) * (1+1) - 1(둘 다 안 입은 경우) = 5
+	
+	*/
+	
+	int n;
+	string name, tag;
+	for(int i = 0; i < t; ++i){
+		
+		map<string,int> m;
+		cin >> n;
+		
+		while(n--){
+			cin >> name >> tag;
+			if(m.find(tag) == m.end()){
+				m.insert({tag,1});
+			}else{
+				m[tag]++;
+			}
+		}
+		
+		int ans = 1;
+		
+		for(auto i : m){
+			ans *= (i.second+1);
+		}
+		ans -= 1;
+		cout << ans << '\n';
+	}
 	
 	return 0;
 }
