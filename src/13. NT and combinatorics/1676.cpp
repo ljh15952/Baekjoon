84a612f0ad609a957343a4bbe26f1e9e540afb00
중화이 <ljh15952@gmail.com>
2022-04-28 10:42:47 +0000

    4/28 19:42

---
+++ src/13. NT and combinatorics/1676.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+	
+	int n;
+	
+	cin >> n;
+	
+	cout << (n / 5) + (n / 25) + (n / 125) << endl;
+	
+	return 0;
+}
