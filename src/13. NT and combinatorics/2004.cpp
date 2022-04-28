614b4930967c057d9cf60c5bc791cfc3f20531bb
중화이 <ljh15952@gmail.com>
2022-04-28 11:06:23 +0000

    4/28 20:06

--- src/13. NT and combinatorics/2004.cpp
+++ src/13. NT and combinatorics/2004.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-typedef long long ll;
 
-pair<ll, ll> PF(int n)
+
+pair<long long int, long long int> PF(int n)
 {
-	pair<ll, ll> res;
+	pair<long long int, long long int> res;
 
 	res.first = 0;
 	res.second = 0;
 
-	for (ll i = 2; i <= n; i *= 2)
+	for (long long int i = 2; i <= n; i *= 2)
 	{
 		res.first += (n / i);
 	}
 
-	for (ll i = 5; i <= n; i *= 5)
+	for (long long int i = 5; i <= n; i *= 5)
 	{
 		res.second += (n / i);
 	}
@@ -29,9 +29,9 @@ int main(){
 	int n, m;
 	cin >> n >> m;
 
-	pair<ll, ll> den1 = PF(n - m), den2 = PF(m), nom = PF(n);
-	ll fives = nom.second - den1.second - den2.second;
-	ll twos = nom.first - den1.first - den2.first;
+	pair<long long int, long long int> den1 = PF(n - m), den2 = PF(m), nom = PF(n);
+	long long int fives = nom.second - den1.second - den2.second;
+	long long int twos = nom.first - den1.first - den2.first;
 	cout << min(fives, twos) << '\n';
 	
 	return 0;
