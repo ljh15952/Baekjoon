2b79cc99e0da83e4b14261e3662a01e15ac39ef8
중화이 <ljh15952@gmail.com>
2022-04-30 04:14:05 +0000

    4/30 13:14

--- src/14. Backtracking/15649.cpp
+++ src/14. Backtracking/15649.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+
+int n,m;
+int arr[9];
+bool visited[9];
+
+void dfs(int k){
+	if(k == m){
+		for(int i = 0; i < m; ++i){
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+	}else{
+		for(int i = 1; i <= n; ++i){
+			if(!visited[i]){
+				visited[i] = true;
+				arr[k] = i;
+				dfs(k+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
+
+
 int main(){
-	
-	cout << "later" << endl;
-	
+	cin >> n >> m;
+	dfs(0);
 	return 0;
 }
