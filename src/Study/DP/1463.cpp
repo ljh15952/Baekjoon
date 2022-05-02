bc73f68602f408e438c3708e18ec65734d647af2
중화이 <ljh15952@gmail.com>
2022-05-02 11:22:58 +0000

    5/2 20:22

--- src/Study/DP/1463.cpp
+++ src/Study/DP/1463.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
 	
+	int n;
+	scanf("%d",&n);
 	
-	cout << "D P start" << endl;
-	cout << "TEST" << endl;
+	int ct = 0;
+	int arr[1000001] = {0,};
+
+	arr[1] = 0;
+	
+	for(int i = 2; i <= n; ++i){
+		arr[i] = arr[i-1] + 1;
+		if(i % 2 == 0)
+			arr[i] = min(arr[i],arr[i/2] + 1);
+		if(i % 3 == 0)
+			arr[i] = min(arr[i],arr[i/3] + 1);
+	}
+	
+	cout << arr[n] << endl;
+	
 	return 0;
 }
