8e8e7b360760e82a6855fa024888c79bde65971f
중화이 <ljh15952@gmail.com>
2022-05-02 10:57:14 +0000

    5/2 19:57

--- src/Study/Easy/10992.cpp
+++ src/Study/Easy/10992.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 int main(){
 	
-	cout << "STAR!" << endl;
+	int n;
+	
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i){
+		for(int j = n-1; j > i; --j){
+			cout << " ";
+		}
+		if(i == n-1){
+			for(int j = 0; j < n*2-1; ++j){
+				cout << "*";
+			}
+		}else{
+			for(int j = 0; j < (i+1)*2-1; ++j){
+				if(j == 0 || j == (i+1)*2-2){
+					cout << "*";
+				}else{
+					cout << " ";
+				}
+			}
+		}
+		cout << endl;
+	}
 	
 	return 0;
 }
