d91be22e721889654727919dfc29341050a3f376
중화이 <ljh15952@gmail.com>
2022-05-03 10:36:13 +0000

    5/3 19:36

--- src/Study/DP/11726.cpp
+++ src/Study/DP/11726.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+
 int main(){
 	
-	cout << "DP2 start" << endl;
+	int n;
+	
+	cin >> n;
+	
+	int dp[1001] = {0,};
+	dp[1] = 1;
+	dp[2] = 2;
+	
+	for(int i = 3; i <= n; ++i){
+		dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+	}
+	
+	cout << dp[n] << endl;
 	
 	return 0;
 }
