d8e7b6f48806b7c21a1ff8311461cb6824d195e0
중화이 <ljh15952@gmail.com>
2022-05-04 10:50:23 +0000

    5/4 19:50

--- src/Study/DP/9095.cpp
+++ src/Study/DP/9095.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+int ct = 0;
+
+void add(int sum, int goal){
+	if(sum > goal){
+		return;
+	}else if(sum == goal){
+		ct++;
+	}else{
+		for(int i = 1; i <= 3; ++i){
+			add(sum + i, goal);
+		}
+	}
+}
+
 int main(){
 	
-	cout << "5/4" << endl;
+	int n;
+	
+	cin >> n;
+	
+	int num;
+	
+	for(int i = 0; i < n; ++i){
+		cin >> num;
+		add(0,num);
+		cout << ct << endl;
+		ct = 0;
+	}
 	
 	return 0;
 }
