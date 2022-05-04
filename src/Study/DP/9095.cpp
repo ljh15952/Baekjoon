0106178523f6a4df61c4ff809ba3105fe7b8c088
중화이 <ljh15952@gmail.com>
2022-05-04 10:57:26 +0000

    5/4 19:57

--- src/Study/DP/9095.cpp
+++ src/Study/DP/9095.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int ct = 0;
 
+/*
 void add(int sum, int goal){
 	if(sum > goal){
 		return;
@@ -15,6 +16,7 @@ void add(int sum, int goal){
 		}
 	}
 }
+*/
 
 int main(){
 	
@@ -24,11 +26,25 @@ int main(){
 	
 	int num;
 	
+	// for(int i = 0; i < n; ++i){
+	// 	cin >> num;
+	// 	add(0,num);
+	// 	cout << ct << endl;
+	// 	ct = 0;
+	// }
+	
+	int dp[11] = {0,};
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	
 	for(int i = 0; i < n; ++i){
 		cin >> num;
-		add(0,num);
-		cout << ct << endl;
-		ct = 0;
+		
+		for(int j = 4; j <= num; ++j){
+			dp[j] = dp[j-1] + dp[j-2] + dp[j - 3];
+		}
+		cout << dp[num];
 	}
 	
 	return 0;
