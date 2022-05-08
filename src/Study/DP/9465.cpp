3eec4be5a889d6045acc5f52f3ff2e702563cff4
중화이 <ljh15952@gmail.com>
2022-05-08 06:21:11 +0000

    5/8 15:21

--- src/Study/DP/9465.cpp
+++ src/Study/DP/9465.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int buf[2][100000];
+int dp[2][100000];
+
+
 int main(){
 	
-	cout << "Fighting!" << endl;
+	int t;
+	cin >> t;
+	
+	for(int k = 0; k < t; ++k){
+		int n;
+		cin >> n;
+
+		int score;
+		for(int i = 0; i < 2; ++i){
+			for(int j = 0; j < n; ++j){
+				scanf("%d",&buf[i][j]);
+			}
+		}
+
+		dp[0][0] = buf[0][0];
+		dp[1][0] = buf[1][0];
+		dp[0][1] = buf[0][1] + dp[1][0];
+		dp[1][1] = buf[1][1] + dp[0][0];
+
+		for (int i = 2; i < n; i++) {
+			dp[0][i] = buf[0][i] + max(dp[1][i-1],dp[1][i-2]);
+			dp[1][i] = buf[1][i] + max(dp[0][i-1],dp[0][i-2]);
+		}
+
+		cout << max(dp[0][n-1],dp[1][n-1]) << endl;
+	}
 	
 	return 0;
 }
