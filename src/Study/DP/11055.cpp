b8b76fa7c56571a99dda0ff8e9dbd3ff16739692
중화이 <ljh15952@gmail.com>
2022-05-14 04:18:48 +0000

    5/14 13:18

--- src/Study/DP/11055.cpp
+++ src/Study/DP/11055.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+int arr[1001];
+int dp[1001];
 int main(){
 	
-	cout << "11055!" << endl;
+	int N;
+	cin >> N;
 	
+	for(int i = 1; i <= N; ++i){
+		cin >> arr[i];
+	}
+	
+	int max = 0;	
+	for(int i = 1; i <= N; ++i){
+		dp[i] = arr[i];
+		for(int j = 1; j < i; ++j){
+			if(arr[j] < arr[i] && dp[i] < dp[j]+arr[i]){
+				dp[i] = dp[j] + arr[i];
+			}
+		}
+		if(max < dp[i])
+			max = dp[i];
+	}
+	
+	cout << max << endl;	
 	return 0;
 }
