e963a85de3ac94ff8d5fed4d9ecfb51204afd5e9
중화이 <ljh15952@gmail.com>
2022-05-16 09:49:59 +0000

    5/16 18:50

--- src/Study/DP/11054.cpp
+++ src/Study/DP/11054.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int arr[1001];
-int dp[1001];
+int dp1[1001];
 int dp2[1001];
 
 int main(){
@@ -15,54 +15,28 @@ int main(){
 	for(int i = 1; i <= N; ++i){
 		cin >> arr[i];
 	}
-	//왜 틀렸는지 모르겠어요 다음에 또 풀게요 
-	//hihi
-	int maxDp1 = 0;
-	int maxDp1_index = 0;
-	int maxDp2 = 0;
-	int maxDp2_index = 0;
 	
 	for(int i = 1; i <= N; ++i){
-		for(int j = 1; j <= i; ++j){
-			if(arr[i] > arr[j]){
-				dp[i] = max(dp[i],dp[j] + 1);
+		dp1[i] = 1;
+		dp2[N-i+1] = 1;
+		for(int j = 1; j < i; ++j){
+			if(arr[i] > arr[j] && dp1[j] + 1 > dp1[i]){
+				dp1[i] = dp1[j] + 1;
 			}
-		}
-		
-		if(maxDp1 < dp[i]){
-			maxDp1 = dp[i];
-			maxDp1_index = i;
-		}
-		
-	}
-	
-	for(int i = N; i >= 1; --i){
-		for(int j = N; j >= i; --j){
-			if(arr[i] > arr[j]){
-				dp2[i] = max(dp2[i],dp2[j] + 1);
+			if(arr[N-i+1] > arr[N-j+1] && dp2[N-j+1] +1 > dp2[N-i+1]){
+				dp2[N-i+1] = dp2[N-j+1] + 1;
 			}
-		}
-		
-		if(maxDp2 < dp2[i]){
-			maxDp2 = dp2[i];
-			maxDp2_index = i;
 		}
 	}
 	
-	// for(int i = 0; i < N; ++i){
-	// 	cout << arr[i] << " " << dp[i] << endl;
-	// }
-	// cout << endl;
-	// for(int i = 0; i < N; ++i){
-	// 	cout << arr[i] << " " << dp2[i] << endl;
-	// }
-	// cout << endl;
-	// cout << maxDp1 << " " << maxDp1_index << endl;
-	// cout << maxDp2 << " " << maxDp2_index << endl;
-
-	cout << max(dp[maxDp1_index]+dp2[maxDp1_index],dp[maxDp2_index]+dp2[maxDp2_index]) + 1 << endl;
+	int maxArr[N+1];
+	int Max = 0;
+	for(int i = 1; i <= N; ++i){
+		maxArr[i] = dp1[i] + dp2[i];
+		Max = max(Max,maxArr[i]);
+	}
 	
-	//cout << *max_element(dp+0,dp+N-1) + *max_element(dp2+0,dp2+N-1) - 1 << endl;
+	cout << Max - 1 << endl;
 	
 	return 0;
 }
