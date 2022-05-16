e09dd8287818c45a53373c713b369dce66401087
중화이 <ljh15952@gmail.com>
2022-05-16 09:54:44 +0000

    5/16 18:54

--- src/Study/DP/1912.cpp
+++ src/Study/DP/1912.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+int arr[100001];
+int dp[100001];
+
 int main(){
 	
-	cout << "HI" << endl;
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i){
+		cin >> arr[i];
+	}
+
+	//연속된 수의 합 중 가장 큰 합을 구하는 문제 많이 어려워 보임
+	
 	
 	return 0;
 }
