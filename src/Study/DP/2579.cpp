c64221d61d3f1401ce80d58586830953dde59087
중화이 <ljh15952@gmail.com>
2022-05-18 11:47:22 +0000

    5/18 20:47 keep

--- src/Study/DP/2579.cpp
+++ src/Study/DP/2579.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
+
+int arr[301];
+int dp[301];
 
 int main(){
 	
-	cout << "HIHI" << endl;
+	//계단 오르기
+	
+	int n;
+	cin >> n;
+	
+	//못풀겠어요 초등부 문제인데;;
+	
+	for(int i = 0; i < n; ++i){
+		cin >> arr[i];
+		dp[i] = arr[i];
+	}
+	
+	//10 20 15 25 10 20
+	//10 65 15 45 10 20
+	//75
+	for(int i = n-1; i >= 2; --i){
+		if(arr[i-1] > arr[i-2]){
+			dp[i-1] += dp[i];
+		}else{
+			dp[i-2] += dp[i];
+			if(i==3){
+				dp[0] += dp[1];
+			}
+			i--;
+		}
+	}
+	
+	cout << *max_element(dp+0,dp+n) << endl;
 	
 	return 0;
 }
