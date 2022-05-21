512d832763094eeeccacde2b9b02e674d81ea10e
중화이 <ljh15952@gmail.com>
2022-05-21 10:10:21 +0000

    5/21 19:10

--- src/Study/DP/9461.cpp
+++ src/Study/DP/9461.cpp
@@ -2,9 +2,49 @@
 
 using namespace std;
 
+long long int dp[101];
+
 int main(){
 	
-	cout << "9461" << endl;
+	int T;
+	cin >> T;
+
 	
+	dp[1] = 1;
+	dp[2] = 1;	
+	dp[3] = 1;	
+	
+	dp[4] = 2;
+	dp[5] = 2;
+	
+	int N;
+	for(int j = 0; j < T; ++j){
+		cin >> N;
+		for(int i = 6; i <= N; ++i){
+			dp[i] = dp[i-1] + dp[i-5];
+		}
+		cout << dp[N] << endl;
+	}
+	
+	/*
+	풀이
+	수열 10까지
+	0 : 1
+	1 : 1
+	2 : 1
+	3 : p[1] + p[2] = 2
+	4 : 2
+	5 : p[2] + p[4] = 3
+	6 : p[5] + p[1] = 4
+	7 : p[6] + p[2] = 5
+	8 : p[7] + p[3] = 7
+	9 : p[8] + p[4] = 9
+	10 : p[9] + p[5] = 12
+	11 : p[10] + p[6] = 16
+	규칙?
+	6이상부터
+	p[n] = p[n-1] + p[n-5];
+	*/
+
 	return 0;
 }
