fd85e5fef0f9fea382628e0c60f4f97dcdc3ec74
중화이 <ljh15952@gmail.com>
2022-06-06 03:51:51 +0000

    6/6 12:51

--- src/Study/More/11652.cpp
+++ src/Study/More/11652.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+long long int arr[100001];
+
 int main(){
 	
-	cout << "내일 풀게요. 훈련;;";
-	cout << "HI? 휴가" << endl;
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i){
+		cin >> arr[i];
+	}
+	
+	sort(arr,arr+n);
+	
+	long long int max = 1;
+	long long int cnt = 1;
+	long long int ans = arr[0];
+	
+	for(int i = 1; i < n; ++i){
+		if(arr[i]==arr[i-1])
+			cnt++;
+		else
+			cnt=1;
+		
+		if(max<cnt){
+			max=cnt;
+			ans=arr[i];
+		}
+	}
+	
+	cout << ans << '\n';
+	
 	return 0;
 }
