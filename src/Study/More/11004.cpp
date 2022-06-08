77d6228cfb7d385c27a9f4fdcb9e5b97789bd53c
중화이 <ljh15952@gmail.com>
2022-06-08 05:54:11 +0000

    6/8 14:54 Wrong

--- src/Study/More/11004.cpp
+++ src/Study/More/11004.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
 	
-	cout << "k번째 수" << endl;
+	int n,k;
+	
+	cin >> n >> k;
+	
+	vector<int> vec;
+	int a;
+	for(int i = 0; i < n; ++i){
+		cin >> a;
+		vec.push_back(a);
+	}
+	
+	sort(vec.begin(),vec.end());
+	
+// 	for(auto it : vec){
+// 		cout << it << endl;
+// 	}
+	
+	cout << vec[k-1] << endl;
 	
 	return 0;
 }
