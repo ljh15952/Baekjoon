bb4448f5c74ac93badcda72a0885e56f01250b3d
중화이 <ljh15952@gmail.com>
2022-06-10 08:02:54 +0000

    6/10 17:02

--- src/Study/More/10828.cpp
+++ src/Study/More/10828.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int Stack[10000];
+
+void push(){
+	
+}
+
+void pop(){
+	
+}
+
+void size(){
+	
+}
+
+void empty(){
+	
+}
+
+void top(){
+	
+}
+
 int main(){
 
-	cout << "hi" << endl;
+	int n;
+	cin >> n;
+	
+	
+	string str;
+	for(int i = 0; i < n; ++i){
+		cin >> str;
+		switch(str){
+			case "push":
+				push();
+				break;
+			case "pop":
+				cout << pop() << endl;
+				break;
+			case "size":
+				cout << size() <<endl;
+				break;
+			case "empty":
+				cout << empty() << endl;
+				break;
+			case "top":
+				cout << top() << endl;
+				break;
+		}
+	}
 
 	return 0;
 }
