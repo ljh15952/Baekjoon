ed06fb4260e164a35a4e337c2ae16b21d7a29d54
중화이 <ljh15952@gmail.com>
2022-06-12 03:39:00 +0000

    6/12 12:38

--- src/Study/More/10799.cpp
+++ src/Study/More/10799.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-	
-	cout << "10799" << endl;
 	//start
 	/*
 	()(((()())(())()))(()) 17
 	
 	(((()(()()))(())()))(()()) 24
+	
+	알고리즘 자신의 괄호 안에 ()이게 있으면 막대 +1
 	*/
+	
+	string str;
+	cin >> str;
+	
+	
 	
 	return 0;
 }
--- src/Study/More/9012.cpp
+++ src/Study/More/9012.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
 
+// bool isVPS(string str){
+// 	int pivot = 0;
+// 	for(int i = str.size()-1; i >= 0; --i){
+// 		if(str[i] == ')'){
+// 			for(int j = i-1; j >= 0; --j){
+// 				if(str[j] == '('){
+// 					str[i] = '*';
+// 					str[j] = '*';
+// 					break;
+// 				}
+// 			}
+// 		}
+// 	}
+// 	for(int i = str.size()-1; i >= 0; --i){
+// 		if(str[i] != '*')
+// 			return false;
+// 	}
+// 	return true;
+// }
 bool isVPS(string str){
-	int pivot = 0;
-	for(int i = str.size()-1; i >= 0; --i){
-		if(str[i] == ')'){
-			for(int j = i-1; j >= 0; --j){
-				if(str[j] == '('){
-					str[i] = '*';
-					str[j] = '*';
-					break;
-				}
-			}
+
+	stack<char> s;
+	
+	for(int i = 0; i < str.size(); ++i){
+		char c = str[i];
+		
+		if(c == '('){
+			s.push(c);
+		}else if(s.empty()){
+			return false;
+		}else{
+			s.pop();
 		}
 	}
-	for(int i = str.size()-1; i >= 0; --i){
-		if(str[i] != '*')
-			return false;
-	}
-	return true;
+	
+	return s.empty();
 }
 
 int main(){
@@ -39,6 +58,6 @@ int main(){
 			cout << "NO" << endl;
 		
 	}
-		
+	
 	return 0;
 }
