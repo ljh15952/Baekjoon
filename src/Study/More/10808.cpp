f72ca5d3b93f0f622a3d5f62172debe9b38a4cd5
중화이 <ljh15952@gmail.com>
2022-06-19 04:46:18 +0000

    6/19 13:46

--- src/Study/More/10808.cpp
+++ src/Study/More/10808.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-
+#include <string>
+#include <map>
 using namespace std;
 
 int main(){
 	
-	cout << "10808" << endl;
+	string s;
+	cin >> s;
+	
+	map<char,int> arr;
+	
+	for(char c = 'a'; c <= 'z'; ++c){
+		arr.insert({c,0});
+	}
+	
+
+	for(int i = 0; i < s.length(); ++i){
+		arr[s[i]]++;
+	}
+	
+	for(auto it : arr){
+		cout << it.second << " ";
+	}
 	
 	return 0;
 }
