bd0028f4f7bd8f4cb55d3eedbf17fe71f44f9909
중화이 <ljh15952@gmail.com>
2022-06-19 06:14:50 +0000

    6/19 15:14

--- src/Study/More/11656.cpp
+++ src/Study/More/11656.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
 int main(){
 	
-	cout << "11656" << endl;
+	string s;
+	cin >> s;
+	
+	vector<string> arr;
+	
+	for(int i = 0; i < s.length(); ++i){
+		arr.push_back(s.substr(i,s.length()));
+	}
+	
+	sort(arr.begin(),arr.end());
+	
+	for(auto it : arr){
+		cout << it << endl;
+	}
 	
 	return 0;
 }
