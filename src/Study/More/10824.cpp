f4e638c30325b1b80f9b2183a7281e3809645e22
중화이 <ljh15952@gmail.com>
2022-06-19 05:47:24 +0000

    6/19 14:47

---
+++ src/Study/More/10824.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "10824" << endl;
+	
+	return 0;
+}
--- src/Study/More/11655.cpp
+++ src/Study/More/11655.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
 	
+	string s;
 	
+	getline(cin,s);
 	
+	for(int i = 0; i < s.length(); ++i){
+		int n;
+		if(s[i] >= 'a' && s[i] <= 'z'){
+			n = (s[i] - 'a' + 13) % 26;
+			cout << char('a'+ n);
+		}
+		else if(s[i] >= 'A' && s[i] <= 'Z'){
+			n = (s[i] - 'A' + 13) % 26;
+			cout << char('A'+ n);
+		}else{
+			cout << s[i];
+		}
+	}
+
 	return 0;
 }
