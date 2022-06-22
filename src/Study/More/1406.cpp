c6638b9d4f5fdb187d954232ea5b126fe4a30aa9
중화이 <ljh15952@gmail.com>
2022-06-22 10:42:47 +0000

    6/22 19:42

--- src/Study/More/1406.cpp
+++ src/Study/More/1406.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
+		
+	string str;
+	cin >> str;
 	
-	cout << "Make text editor?" << endl;
+	int n;
+	cin >> n;
+	
+	int cursor = str.length();
+	
+	for(int i = 0 ; i < n; ++i){
+		char c;
+		cin >> c;
+		if(c == 'L'){
+			cursor = max(0, cursor-1);
+		}else if(c == 'D'){
+			int leng = str.length();
+			cursor = min(leng, cursor+1);
+		}else if(c == 'B'){
+			if(cursor > 0){
+				str.erase(str.begin()+cursor-1);
+				cursor--;
+			}
+		}else if(c == 'P'){
+			string s;
+			cin >> s;
+			str.insert(cursor,s);
+			cursor++;
+		}
+	}
+	//ㅈ빠지게 했더니 시간 초과 뜨네"?
+	cout << str << endl;
 	
 	return 0;
 }
