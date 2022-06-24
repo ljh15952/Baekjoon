d6c5227dccdf2ea9192ed9fc4f6e9ad736d72250
중화이 <ljh15952@gmail.com>
2022-06-24 11:10:55 +0000

    6/24 20:10

--- src/Study/More/1158.cpp
+++ src/Study/More/1158.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-class Node{
-	public:
-		Node(int i) : data(i) {};
-		int data;
-		Node * prev = nullptr;
-		Node * next = nullptr;
-};
 
 int main(){
 	
-	int k,n;
+	int n,k;
 	cin >> n >> k;
 	
- 	Node * head = nullptr;
-	Node * tail = nullptr;
+	queue<int> que;
+	
 	for(int i = 1; i <= n; ++i){
-		if(head == nullptr){
-			head = new Node(i);
-			tail = head;			
-		}else{
-			Node * node = new Node(i);
-			tail->next = node;
-			node->prev = tail;
-			tail = node;
-		}
-	}
-	tail->next = head;
-	head->prev = tail;
-
-	Node * node = head;
-	for(int i = 0; i < n; ++i){
-		for(int j = 0; j < k-1; ++j){
-			node = node->next;
-		}
-		cout << node->data << endl;
-		node->prev->next = node->next;
-		node->next->prev = node->prev;
-		node = node->next;
+		que.push(i);
 	}
 	
+	cout << "<";
 	
+	while(que.size()){
+		if(que.size () == 1){
+			cout << que.front() << ">";
+			que.pop();
+			break;
+		}
+		for(int j = 1; j < k; ++j){
+			que.push(que.front());
+			que.pop();
+		}
+		cout << que.front() << ", ";
+		que.pop();
+	}
+	
+	cout << endl;
 	
 	return 0;
 }
