6027c5179cf419333cb158194adf77974b0021e5
중화이 <ljh15952@gmail.com>
2022-06-27 11:06:31 +0000

    6/27 20:06

--- src/Study/More/1850.cpp
+++ src/Study/More/1850.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+int gcd(long long int a, long long int b){
+	if(b == 0)
+		return a;
+	else
+		return gcd(b , a%b);
+}
+
 int main(){
 	
-	cout << "1850" << endl;
+	long long int a,b;
+	cin >> a >> b;
+	
+	for(int i = 0; i < gcd(a,b); ++i){
+		cout << "1";
+	}
+
+	cout << endl;
 	
 	return 0;
 }
