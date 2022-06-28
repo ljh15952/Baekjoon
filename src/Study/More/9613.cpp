12df9e1270dd2fe634ebb1729a3c4cc908a76865
중화이 <ljh15952@gmail.com>
2022-06-28 10:20:05 +0000

    6/28 19:19 wrong

--- src/Study/More/9613.cpp
+++ src/Study/More/9613.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int GCD(int a, int b){
+	if(b == 0)
+		return a;
+	else
+		return GCD(b,a%b);
+}
 
 int main(){
 	
 	int t;
-	
-	
 	/*
 	
 	10 20 30 40
@@ -25,13 +31,28 @@ int main(){
 	------->70
 	
 	*/
+	
+	cin >> t;
+	
 	for(int i = 0; i < t; ++i){
 		int n;
 		cin >> n;
+		vector<int> arr;
+		int ans = 0; //답
 		for(int j = 0; j < n; ++j){
-			
+			int l;
+			cin >> l;
+			arr.push_back(l);
 		}
+		for(int j = 0; j < n; ++j){
+			for(int k = j+1; k < n; ++k){
+				ans += GCD(arr[j],arr[k]);
+			}
+		}
+		cout << ans << endl;
 	}
+	
+	
 	
 	
 	return 0;
