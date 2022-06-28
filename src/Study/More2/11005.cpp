294fa2b40d97f259f28cc3fea0d0f457d433ba2b
중화이 <ljh15952@gmail.com>
2022-06-28 11:05:08 +0000

    6/28 20:05

---
+++ src/Study/More2/11005.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	long long int n;
+	
+	cin >> n;
+	
+	int b;
+	cin >> b;
+	
+	/*
+	
+	A : 10
+	B : 11
+	C : 12
+	D : 13
+	E : 14
+	.
+	.
+	.
+	Y : 34
+	Z : 35
+	
+	ex) 2진법
+	00 01 10 11 100 101 110 111 1000..
+	3진법
+	1 2 3 10 11 12 13 101
+	
+	9를 2진법으로?
+	9->1001
+	9 - 2
+	
+	*/
+	
+
+	
+	
+	return 0;
+}
