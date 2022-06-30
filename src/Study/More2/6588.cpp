4f1fe45ede0597760842e8fbe69bffd30922fc93
중화이 <ljh15952@gmail.com>
2022-06-30 10:11:59 +0000

    6/30 19:11

--- src/Study/More2/6588.cpp
+++ src/Study/More2/6588.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
 
 using namespace std;
 
 int main(){
 	
-	int arr[10001] = {0,};
+	int arr[1000001]={1,1,1,0,};
 	arr[1] = 1;
 	
-	for(int i = 6; i <= 1000000; ++i){
-		for(int j = 6; j * i <= 1000000; ++j){
+	for(int i = 2; i <= 1000000; ++i){
+		for(int j = 2; j * i <= 1000000; ++j){
 			arr[i*j] = 1;
 		}
 	}
 	
-	for(int i = 0; i < 100; ++i){
-		cout << arr[i] << endl;
+	cin.tie(NULL);
+ 	ios_base::sync_with_stdio(0);
+	
+	int n,left,right;
+	
+	while(1){
+		cin >> n;
+		if(!n) break;
+		
+		left = 3;
+		right = n - 3;
+		
+		while(left <= right){
+			if(!arr[left] && !arr[right]){
+				if(left + right == n)
+					break;
+			}
+			left += 2;
+			right -= 2;
+		}
+		
+		if(left>right)  
+			cout<<"Goldbach's conjecture is wrong.";
+    	else 
+			cout<<n<<" = "<<left<<" + "<<right<<'\n';
 	}
 	
 	return 0;
