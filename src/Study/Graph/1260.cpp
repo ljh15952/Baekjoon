c727e270742996cb0a9db79f24675f5d96900f7c
중화이 <ljh15952@gmail.com>
2022-07-03 11:30:56 +0000

    7/3 20:30

--- src/Study/Graph/1260.cpp
+++ src/Study/Graph/1260.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int n,m,v;
 vector<int> graph[10001];
+bool visited_1[1001];
+bool visited_2[1001];
 
 void dfs(int k){
 	
+	cout << k << " ";
+	visited_1[k] = true;
+	
+	for(int i = 0; i < graph[k].size(); ++i){
+		int now = graph[k][i];
+		if(visited_1[now] == false)
+			dfs(now);
+	}
 }
 
 void bfs(int k){
 	
+	queue<int> q;
+	q.push(k);
+	visited_2[k] = true;
+	
+	while(!q.empty()){
+		
+		int x = q.front();
+		q.pop();
+		cout << x << " ";
+		
+		for(int i = 0; i < graph[x].size(); ++i){
+			int now = graph[x][i];
+			if(visited_2[now] == false){
+				q.push(now);
+				visited_2[now] = true;
+			}
+		}
+		
+	}
 }
 
 int main(){
@@ -24,6 +54,10 @@ int main(){
 		cin >> s >> e;
 		graph[s].push_back(e);
 		graph[e].push_back(s);
+	}
+	
+	for(int i = 1; i < m; ++i){
+		sort(graph[i].begin(),graph[i].end());
 	}
 	
 	dfs(v);
