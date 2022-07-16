7ef15e5b1c1634936860cbe704fc0a58607dbd27
중화이 <ljh15952@gmail.com>
2022-07-16 05:42:26 +0000

    7/16 14:42

--- src/Study/Graph/10451.cpp
+++ src/Study/Graph/10451.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <vecotr>
 using namespace std;
+
+vector<int> graph[1001];
+bool visited[1000];
 
 int main(){
 	
@@ -9,6 +12,15 @@ int main(){
 	
 	
 	*/
+	
+	//int t; //테스트 케이스의 개수
+	//cin >> t;
+	int n; // 순열의 크기
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i){
+		
+	}
 	
 	return 0;
 }
