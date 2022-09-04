2030a50be10c806905a68c7cf886fce9ecfd8ee6
ljh15952 <ljh15952@gmail.com>
2022-09-04 20:13:06 +0900

    9/4 20:12

--- src/Study_Python/Graph/4963.py
+++ src/Study_Python/Graph/4963.py
@@ -7,11 +7,40 @@ w = 2
 h = 2
 
 grid = []
-
+visited = []
 
 for _ in range(h):
     grid.append(list(map(int, input().split())))
 
+for i in range(h):
+    visited.append([])
+    for _ in range(w):
+        visited[i].append(False)
+   
+
+
+
+def dfs(y, x) :
+    print(y,x)
+    visited[y][x] = True
+
+    if x+1 < w:
+        if grid[y][x+1] == 1:
+            dfs(y,x+1)
+    elif x-1 >= 0:
+        if grid[y][x-1] == 1:
+            dfs(y,x-1)
+    elif y+1 < h:
+        if grid[y+1][x] == 1:
+            dfs(y+1,x)
+    elif y-1 >= 0:
+        if grid[y-1][x] == 1:
+            dfs(y-1,x)
+
+
+dfs(0,0) # h, w
+
+print(visited)
 
 
 
