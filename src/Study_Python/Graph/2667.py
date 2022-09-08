e826dad9e637bee756a9b695c014a581eea9da33
ljh15952 <ljh15952@gmail.com>
2022-09-08 22:56:49 +0900

    9/8 22:56

--- src/Study_Python/Graph/2667.py
+++ src/Study_Python/Graph/2667.py
@@ -1,1 +1,49 @@
-print("hi 단지번호 붙이기 문제 Python222")
+from collections import deque
+
+n = int(input()) # 지도의 크기
+
+grid = []
+for _ in range(n):
+    grid.append(list(map(int,input())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+
+
+def bfs(y, x):
+    grid[y][x] = 0 # 들렸다는 체크
+    _deque = deque()
+    _deque.append((y,x))
+
+    cnt = 1
+
+    while _deque:
+        y,x = _deque.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if grid[ny][nx] == 1:
+                grid[ny][nx] = 2
+                _deque.append((ny,nx))
+                cnt += 1
+    return cnt
+
+
+house = []
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            house.append(bfs(i, j))
+
+print(len(house))
+house.sort()
+for i in range(len(house)):
+    print(house[i])
+
+
