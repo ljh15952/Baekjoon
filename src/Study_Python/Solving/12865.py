366e61d658a82f976032eaefcae20393fe38d2b2
ljh15952 <ljh15952@gmail.com>
2022-09-20 23:48:41 +0900

    9/20 23:48

--- src/Study_Python/Solving/12865.py
+++ src/Study_Python/Solving/12865.py
@@ -1,1 +1,13 @@
-print("12865.py")
+n, k = map(int, input().split())
+
+arr = []
+
+for _ in range(100000):
+    arr.append(0)
+
+n1, n2 = 0,0
+for _ in range(n):
+    n1, n2 = map(int, input().split())
+    arr[n1] = n2
+  
+print(n1)
