a50b4bd5b3bf3a103e75fce0704073b02ed84dea
ljh15952 <ljh15952@gmail.com>
2022-09-21 23:57:30 +0900

    9/21 23:57

--- src/Study_Python/Solving/12865.py
+++ src/Study_Python/Solving/12865.py
@@ -1,18 +1,24 @@
 n, k = map(int, input().split())
 
-arr = []
-ans = []
-for _ in range(100000):
-    ans.append(0)
-    arr.append(0)
+arr = [[0,0]]
+knapsack = [[0 for _ in range(k+1)] for _ in range(n+1)]
 
-n1, n2 = 0,0
+
 for _ in range(n):
-    n1, n2 = map(int, input().split())
-    arr[n1] = n2
+    arr.append(list(map(int,input().split())))
 
-ans[1] = arr[1]
+#print(knapsack)
+#print(arr)
 
-for i in range(2,10):
-    ans[i] = arr[i]
-    print(ans[i])
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        weight = arr[i][0]
+        value = arr[i][1]
+        #print(str(weight) + " " + str(value))
+
+        if j < weight:
+            knapsack[i][j] = knapsack[i - 1][j]
+        else:
+            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+
+print(knapsack[n][k])
