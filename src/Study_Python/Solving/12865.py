d1a05a2b36e746e77af42a0d0044f8c2d0c69777
ljh15952 <ljh15952@gmail.com>
2022-09-21 23:30:19 +0900

    9/21 23:30

--- src/Study_Python/Solving/12865.py
+++ src/Study_Python/Solving/12865.py
@@ -11,18 +11,8 @@ for _ in range(n):
     n1, n2 = map(int, input().split())
     arr[n1] = n2
 
-
+ans[1] = arr[1]
 
 for i in range(2,10):
-    if i + i - 1 > k:
-       # print(i)
-        ans[i] = ans[i - 1] + arr[k - i]
-        
-    else:
-        #print(i)
-        ans[i] = ans[i-1] + arr[i-2]
-        #print(arr[i-2])
-#print(ans[2])
-for i in range(0,20):
-    print(arr[i])
-#print(max(ans))
+    ans[i] = arr[i]
+    print(ans[i])
