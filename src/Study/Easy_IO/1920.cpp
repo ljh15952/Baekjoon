99653bbc059f97e8137a1d8d7706263bb5bd085b
중화이 <ljh15952@gmail.com>
2022-09-24 06:13:47 +0000

    Binary search

--- src/Study/Easy_IO/1920.cpp
+++ src/Study/Easy_IO/1920.cpp
@@ -5,15 +5,28 @@
 using namespace std;
 
 int n, m;
-inr arr[100001];
+int t;
+int arr[100001];
 
-int search(int n){
+int search(int num){
 	//TODO 이분탐색
+	int start = 0;
+	int end = n - 1;
 	
+	while(start <= end){
+		int mid = (start + end) / 2;
+		if(arr[mid] == num)
+			return 1;
+		else if(num < arr[mid])
+			end = mid - 1;
+		else 
+			start = mid + 1;
+	}
+	return 0;
 }
 
 int main(){
-	
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
 	
 	for(int i = 0; i < n; ++i){
@@ -25,10 +38,8 @@ int main(){
 	cin >> m;
 		
 	for(int i = 0; i < m; ++i){
-		int t;
 		cin >> t;
-		
-		cout << search(t) << endl;;
+		cout << search(t) << "\n";
 	}
 	
 	return 0;
