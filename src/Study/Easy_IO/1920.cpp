b7358938ed44026ad0222c1e7527cd80b47c935a
중화이 <ljh15952@gmail.com>
2022-09-24 05:18:50 +0000

    9/24 2:18

--- src/Study/Easy_IO/1920.cpp
+++ src/Study/Easy_IO/1920.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int n, m;
+inr arr[100001];
+
+int search(int n){
+	//TODO 이분탐색
+	
+}
+
 int main(){
 	
-	cout << "1920" << endl;
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i){
+		cin >> arr[i];
+	}
+	
+	sort(arr, arr + n);
+	
+	cin >> m;
+		
+	for(int i = 0; i < m; ++i){
+		int t;
+		cin >> t;
+		
+		cout << search(t) << endl;;
+	}
 	
 	return 0;
 }
