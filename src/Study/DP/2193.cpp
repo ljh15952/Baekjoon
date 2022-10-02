73dfc72ca8ba538746353b798d7fdc789ca206cf
중화이 <ljh15952@gmail.com>
2022-10-02 10:44:23 +0000

    10/2 19:44

--- src/Study/DP/2193.cpp
+++ src/Study/DP/2193.cpp
@@ -2,9 +2,43 @@
 
 using namespace std;
 
+long long int arr[91];
+
 int main(){
 	
-	cout << "ASD" << endl;
+	int n;
+	
+	scanf("%d",&n);
+	
+	arr[1] = 1;
+	arr[2] = 1;
+	
+	for(int i = 3; i <= n; ++i){
+		arr[i] = arr[i - 2] + arr[i - 1];
+	}
+	
+	cout << arr[n] << endl;
+	
 	
 	return 0;
 }
+
+/*
+1
+
+10
+
+101
+100
+
+1010
+1000
+1001
+
+10100
+10101
+10001
+10000
+10010
+
+*/
