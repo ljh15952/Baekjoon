01ee0c39ffadd1127e5fe6c3543cd24f1d795619
중화이 <ljh15952@gmail.com>
2022-10-23 10:53:24 +0000

    10/23 19:53

--- src/Study/Tree/1967.cpp
+++ src/Study/Tree/1967.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
+#include <vector>
+//#include <pair>
+#include <memory.h>
 
 using namespace std;
 
-class Node{
-	public:
-		int name;
-};
+vector<pair<int,int>> node[10002];
+bool visited[10002];
 
-class Edge{
-	public:
-		
-};
+int result = 0;
+int endPoint = 0;
+
+void dfs(int p, int len){
+	if(visited[p])
+		return;
+	
+	visited[p] = true;
+	if(result < len){
+		result = len;
+		endPoint = p;
+	}
+	
+	for(int i = 0; i < node[p].size(); i++){
+		dfs(node[p][i].first, len + node[p][i].second);
+	}
+	
+}
 
 int main(){
 	
 	int n;
+	cin >> n;
 	
-	for(int i = 0; i < n-1; ++i){
-		
+	for(int i = 0; i < n-1; i++){
+		int a,b,c;
+		cin >> a >> b >> c;
+		node[a].push_back({b,c});
+		node[b].push_back({a,c});
 	}
+	
+	dfs(1,0);
+	
+	result = 0;
+	memset(visited, false, sizeof(visited));
+	
+	dfs(endPoint, 0);
+	
+	cout << result;
 	
 	return 0;
 }
