f405a7ba765834fb7b937ee65aea9e26c4610de2
중화이 <ljh15952@gmail.com>
2022-11-19 11:45:47 +0000

    20:45

---
+++ src/Study/Tree/2263.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "2263" << endl;
+	
+	return 0;
+}
+
+/*
+트리의 순회
+n개의 정점을 갖는 이진 트리의 정점에 1부터 n까지의 번호가 중복 없이 매겨져 있다.
+이와 같은 이진 트리의 인오더와 포스트오더가 주어졌을 때, 프리오더를 구하는 프로그램 작성
+
+입력
+n(1<=n<=100000)
+인오더 n개의 자연수
+포스트오더 n개의 자연수
+
+출력
+첫째 줄에 프리오더를 출력
+
+인오더 = left root right
+포스트오더 = left right root
+프리오더 = root left right
+
+*/
