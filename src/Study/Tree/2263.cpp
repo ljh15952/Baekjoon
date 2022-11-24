4256fa13d4b898322071205ff67ab198a6d67b7f
중화이 <ljh15952@gmail.com>
2022-11-24 11:13:02 +0000

    11/24 20:13

--- src/Study/Tree/2263.cpp
+++ src/Study/Tree/2263.cpp
@@ -8,8 +8,22 @@ int postorder[100001];
 int n;
 
 
-void getPreOrder(int inStart, int inEnd, int postStart, int postEnd){
-	
+// preorder 를 출력하는 함수 (재귀)
+void getPreOrder(int inStart, int inEnd, int postStart, int postEnd)
+{
+    // 더 이상 분할 할 수 없는 경우 return
+    if (inStart > inEnd || postStart > postEnd)
+        return;
+    // postorder의 끝 값이 root값
+    // Index 배열을 통해 inorder에서의 root index를 쉽게 구할 수 있다.
+    int rootIndex = Index[postorder[postEnd]];
+    int leftSize = rootIndex - inStart;
+    int rightSize = inEnd - rootIndex;
+    cout << inorder[rootIndex] << ' '; // root 값 출력
+
+    // 재귀 함수 호출
+    getPreOrder(inStart, rootIndex - 1, postStart, postStart + leftSize - 1);
+    getPreOrder(rootIndex + 1, inEnd, postStart + leftSize, postEnd - 1);
 }
 
 int main(){
@@ -22,6 +36,8 @@ int main(){
     }
     for (int i = 1; i <= n; i++)
         cin >> postorder[i];
+	
+	getPreOrder(1, n, 1, n);
 	
 	return 0;
 }
