51d83e8c06efe03e1ae51696c1c3466c0fefda72
LeeJeungHwa <ljh15952@gmail.com>
2022-12-01 22:06:19 +0900

    Create 1966re.cpp

---
+++ src/toney9402/Data_structure/1966re.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	
+	
+	
+	return 0;
+}
+
+/*
+
+4(문서의 수) 2(궁금한 문서의 위치 0부터 시작)
+1 2 3 4 (문서의 중요도)
+
+문서 
+1 2 3 4 5 6
+1 1 9 1 1 1
+
+
+
+
+
+
+*/
