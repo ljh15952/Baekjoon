3d75c9a10342733853fea4b803beabf83ab6ec0a
LeeJeungHwa <ljh15952@gmail.com>
2022-12-01 22:32:15 +0900

    Update 1966re.cpp
    re

--- src/toney9402/Data_structure/1966re.cpp
+++ src/toney9402/Data_structure/1966re.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
 int main() {
-	
-	
-	
+	int n,c;
+	cin >> n >> c;
+
+
+	queue<pair<int, int>> q;
+	priority_queue<int> pq;
+
+	for(int i = 0; i < n; ++i){
+		int t;
+		cin >> t;
+		q.push({i, t});
+		pq.push(t);
+	}
+	int ct = 0;
+	while(!pq.empty()){
+		int idx = q.front().first;
+		int val = q.front().second;
+		
+		q.pop();
+		if(val == pq.top()){
+		    pq.pop();
+		    ct++;
+		    if(idx == c){
+		        cout << ct << endl;
+		        break;
+		    }
+		}else{
+		    q.push({idx,val});
+		}
+	}
 	return 0;
 }
-
-/*
-
-4(문서의 수) 2(궁금한 문서의 위치 0부터 시작)
-1 2 3 4 (문서의 중요도)
-
-문서 
-1 2 3 4 5 6
-1 1 9 1 1 1
-
-
-
-
-
-
-*/
