b5062b6e692eb10f89b0c73ed6ff741fc61fc7cc
중화이 <ljh15952@gmail.com>
2022-12-09 11:27:12 +0000

    20:27

--- src/toney9402/Data_structure/2504.cpp
+++ src/toney9402/Data_structure/2504.cpp
@@ -1,36 +1,54 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
+#include <stack>
 using namespace std;
 
 int main(){
 	
 	string str;
 	cin >> str;
-	cout << str << endl;
-	
-	vector<char> stack;
-	
-	for(int i = 0; i < str.length; ++i){
-		
-	}
-	
-	return 0;
+    
+    stack<char>	arr;
+    stack<char> math;
+    
+    for(auto it : str){
+        
+        if(arr.empty() == true){
+            arr.push(it);
+        }else{
+            if((it == '(' || it == '[') && (arr.top() == '(' || arr.top() == '[')){
+                math.push('x');
+            }else if((arr.top() == ')' || arr.top() == ']') && (it == '(' || it == '[')){
+                math.push('+');
+            }
+            if((arr.top() == '(' || it == ')') && (arr.top() == '[' && it == ']')){
+                arr.pop();
+            }else{
+                arr.push(it);
+            }   
+        }
+        if(it == '('){
+            math.push('2');
+        }else if(it == '['){
+            math.push('3');
+        }
+    }
+    if(arr.empty() == false){
+        cout << "0" << endl;
+        
+        while(!arr.empty()){
+            cout << arr.top() << " ";
+            arr.pop();
+        }
+        cout << endl;   
+        //return 0;
+    }
+    
+    while(!math.empty()){
+        cout << math.top() << " ";
+        math.pop();
+    }
+        
+        
+    return 0;
 }
-
-/*
-
-스택 넣고 다음거 넣을 때 방금 넣은거랑 짝이면 pop 두번 -> 마지막에 남은 사이즈가 1이상이면 올바르지 못한 괄호열이다.
-
-여는 괄호 뒤 여는 괄호가 한번 더 나오면 곱하기
-닫는 괄호 뒤 여는 괄호가 더 나오면 더한다
-
-지금 까지의 연산을 자료구조의 저장 후 순서대로 실행 한다면?
-
-(()[[]])
-(2+3x3) x 2 = 22
-
-2 x 2 + 3 x 3 아ㅋㅋ 이거도 스택이네 뒤에서 부터 뽑아서 계산!!
-
-*/
