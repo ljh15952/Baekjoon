61b4df929893b6653161f7da0302bd3ce3a5a3a8
LeeJeungHwa <ljh15952@gmail.com>
2023-01-09 23:39:37 +0900

    10093

---
+++ Workbook/Basic code/10093.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    long long A, B;
+    cin >> A >> B;
+    
+    if(A > B) swap(A,B);
+    
+    
+    if(B == A) cout << '0';
+    else{
+        cout << B - A - 1 << '\n';
+        for(long long i = A+1; i < B; ++i){
+            cout << i << ' ';
+        }
+    }
+    
+    return 0;
+}
