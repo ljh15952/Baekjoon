5b0f3d7be0ef1f20a7b74ebb03e38e55449b0ca4
중화이 <ljh15952@gmail.com>
2023-01-12 14:30:26 +0000

    23:30

--- Workbook/Basic code/2309.cpp
+++ Workbook/Basic code/2309.cpp
@@ -11,26 +11,25 @@ int main(){
 
 	
 	for(int a = 0; a < 9; ++a){
-		int total = 0;
 		for(int b = a + 1; b < 9; ++b){
-			total = 0;
-			
-			int i = 0;
+			int sum = 0, idx = 0;
 			for(int c = 0; c < 9; ++c){
 				if(c != a && c != b){
-					result[i++] = num[c];
+					sum += num[c];
+					result[idx++] = num[c];
 				}
 			}
-			for(int i = 0; i < 7; ++i) total += result[i];
 			
-			if(total == 100) break;
+			if(sum == 100){
+				sort(result,result+7);
+				for(int i = 0; i < 7; ++i){
+					cout << result[i] << "\n";
+				}
+				return 0;
+			}
+			
 		}
-		if(total == 100) break;
 	}
-	
-	sort(result, result+7);
-	for(int i = 0; i < 7; ++i)
-		cout << result[i] << "\n";
 	
 	/*
 	9명의 난쟁이들
