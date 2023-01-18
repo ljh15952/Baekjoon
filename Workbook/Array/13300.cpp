ef558a2f44d67aeadfe4bd8941ae22db9ae8a067
중화이 <ljh15952@gmail.com>
2023-01-18 11:50:48 +0000

    20:50

--- Workbook/Array/13300.cpp
+++ Workbook/Array/13300.cpp
@@ -5,7 +5,13 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "qew" << '\n';
+	int N, K;
+	cin >> N >> K;
+	int S, Y;
+	for(int i = 0; i < N; ++i){
+		cin >> S >> Y;
+		
+	}
 	
 	
 	return 0;
