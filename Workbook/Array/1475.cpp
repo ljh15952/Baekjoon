e49bcdd72ba4023221dbe29541f88d159db26b30
중화이 <ljh15952@gmail.com>
2023-01-20 11:47:49 +0000

    20:47

--- Workbook/Array/1475.cpp
+++ Workbook/Array/1475.cpp
@@ -5,7 +5,24 @@ int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
+	int arr[11] = {};
 	
+	string N;
+	cin >> N;
+	
+	for(int it : N){
+		arr[it - '0']++;
+	}
+	
+	int ans = 0;
+	
+	for(int i = 0; i < 10; i++){
+		if(i == 6 || i == 9) continue;
+		ans = max(ans, arr[i]);
+	}
+	
+	ans = max(ans, (arr[6]+arr[9]+1)/2);
+  cout << ans;
 	
 	return 0;
 }
