d93015b0a9cac805f42e2c572ccfa18dab8162c9
중화이 <ljh15952@gmail.com>
2023-01-21 04:38:32 +0000

    3273 9712kb 12ms

--- Workbook/Array/3273.cpp
+++ Workbook/Array/3273.cpp
@@ -9,17 +9,24 @@ int main(){
 	int n;
 	cin >> n;
 	
-	int arr[n] = {};
+	int arr[2000001] = {};
+	int a;
 	for(int i = 0; i < n; ++i){
-		cin >> arr[i];
+		cin >> a;
+		arr[a] = 1;
 	}
 	
 	int x;
 	cin >> x;
 	
 	int ans = 0;
-	for(int i = 0; i < n; ++i){
-		if(arr[i] + arr[x - i] == x) ans++;
+	for(int i = 1; i <= 2000001; ++i){
+		if(x - i <= 0) continue; 
+		if(i == x - i) continue;
+		if(arr[i] != 1	|| arr[x-i] != 1) continue;
+	
+		arr[i] = 0;
+		ans++;
 	}
 	
 	cout << ans << '\n';
