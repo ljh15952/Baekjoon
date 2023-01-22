a1fe304551df02c916984d8c46b36015190986c7
중화이 <ljh15952@gmail.com>
2023-01-22 12:41:30 +0000

    21:41

--- Workbook/LinkedList/1158.cpp
+++ Workbook/LinkedList/1158.cpp
@@ -2,40 +2,35 @@
 
 using namespace std;
 
-class Node{
-	public:
-		Node * next = nullptr;
-		int data;
-};
-
-class List{
-	public:
-		Node * head = nullptr;
-		Node * tail = nullptr;
-		void insert(Node * n){
-			if(head == nullptr){
-				head = n;
-				tail = n;
-			}else{
-				tail->next = n;
-				tail = n;
-			}
-		}
-};
+int N, K;
+queue<int> q;
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-	List * li = new List();
-	
-	int N, K;
 	cin >> N >> K;
 	
 	for(int i = 1; i <= N; ++i){
-		
+		q.push(i);
 	}
 	
+	cout << '<';
+	while(1){
+		for(int i = 0; i < K-1; ++i){
+			q.push(q.front());
+			q.pop();
+		}
+			
+		if(q.size() <= 1){
+			cout << q.front() << ">\n";
+			break;
+		}else{
+			cout << q.front() << ", ";
+			q.pop();
+		}
+	}
+		
 	return 0;
 }
 
@@ -45,5 +40,6 @@ int main(){
 이 과정을 계속해 나간다. N명 사람이 모두 제거 될 때까지 계속된다
 원에서 사람들이 제거되는 순서를 (N,K)요세푸스 순열이라고 한다.
 (7, 3)요세푸스 순열
-<3, 6, 2, 7, 5, 2, 4>
+1 2 3 4 5 6 7
+<3, 6, 2, 7, 5, 2, 1, 4>
 */
