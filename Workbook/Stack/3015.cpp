b40c2e11a272570041dc6a1810f1e26b8d1eaf99
중화이 <ljh15952@gmail.com>
2023-01-30 11:27:50 +0000

    20:27

--- Workbook/Stack/3015.cpp
+++ Workbook/Stack/3015.cpp
@@ -3,8 +3,30 @@
 using namespace std;
 
 int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "3015" << '\n';
+	int N;
+	cin >> N;
+	
+	
+	
+	
 	
 	return 0;
 }
+
+/*
+0 1 2 3 4 5 6
+2 4 1 2 2 5 1
+
+6 -> 1
+5 -> 3
+4 -> 2
+3 -> 2
+2 -> 1
+1 -> 1
+0 -> 0
+-> 10명
+
+
+*/
