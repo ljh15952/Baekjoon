9d8bde3e2bedd40ce46d3a8784135f37465e054c
중화이 <ljh15952@gmail.com>
2023-01-31 15:05:27 +0000

    12:05 pass

---
+++ Workbook/Stack/6549.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	
+	
+	return 0;
+}
+
+/*
+너무 어렵네용 패스
+https://www.acmicpc.net/problem/6549
+*/
