5e0dd67f216affe1494168e003cdbc228583a6d2
중화이 <ljh15952@gmail.com>
2023-02-05 11:51:45 +0000

    wrong ans

--- Workbook/Deque/11003.cpp
+++ Workbook/Deque/11003.cpp
@@ -5,8 +5,36 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QE" << '\n';
+	int N, L;
+	cin >> N >> L;
 	
+	deque<int> dq;
 	
+	for(int i = 0; i < N; ++i){
+		int a;
+		cin >> a;
+		dq.push_back(a);
+		
+		if(i >= L){
+			dq.pop_front();
+		}
+		cout << *min_element(dq.begin(),dq.end()) << ' ';
+	}
+	cout << '\n';
 	return 0;
 }
+
+/*
+N개의 수 와 L이 주어짐
+Di = Ai - L+1 ~ Ai 중 최소값
+D에 저장된 수를 출력하는 프로그램
+i <= 0 인 Ai는 무시하고 D
+
+1 5 2 3 6 2 3 7 3 5 2  6
+0 1 2 3 4 5 6 7 8 9 10 11
+1 1 1 2 2 2 2 2 3 3 2  2
+L = 3
+
+D0 = A0-L+1 ~ A11 최소값
+
+*/
