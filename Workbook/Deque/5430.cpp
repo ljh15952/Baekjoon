a07f28ce79a50741e7d040a6228e6aba53e6dbda
중화이 <ljh15952@gmail.com>
2023-02-06 11:51:11 +0000

    5430.cpp

---
+++ Workbook/Deque/5430.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "QEW" << '\n';
+	
+	return 0;
+}
+
+/*
+AC언어 
+정수 배열에 연산을 하기 위해 만든 언어
+두 다지 함수 R(뒤집기), D(버리기)
+
+배열이 비어있는데 D함수를 사용하면 에러가 발생
+
+RDD -> 배열을 뒤집고 처음 두 수를 버림
+
+배열의 초기값과 수행할 함수가 주어지고 최종 결과를 구하는 프로그램 
+
+에러가 발생할 경우에는 error를 출력한다.
+
+*/
