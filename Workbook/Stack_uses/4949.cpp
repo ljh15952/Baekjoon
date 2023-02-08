34cbfeb84f0f98f249a0a5e2f529788bce64b434
중화이 <ljh15952@gmail.com>
2023-02-08 11:44:19 +0000

    2044

--- Workbook/Stack_uses/4949.cpp
+++ Workbook/Stack_uses/4949.cpp
@@ -5,7 +5,14 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "4949.cpp:" << '\n';
+	string s;
+	
+	while(1){
+		cin >> s;
+		if(s == '.') break;
+		
+		cout << "pass";
+	}
 	
 	
 	return 0;
