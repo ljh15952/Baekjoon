89ca0a6b396309f2ee7af6c0484041ce151e4afd
중화이 <ljh15952@gmail.com>
2023-02-15 11:13:01 +0000

    BFS COde

---
+++ Workbook/BFS/BFS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define X first
+#define Y second
+
+int board[502][502] = {...};
+bool vis[502][502];
+
+int n = 7, m = 10;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	
+}
