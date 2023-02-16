02c3d30d66d8f912fbb5c62301ea4b282c4b44e0
중화이 <ljh15952@gmail.com>
2023-02-16 11:28:02 +0000

    20:30

--- Workbook/BFS/1926.cpp
+++ Workbook/BFS/1926.cpp
@@ -2,10 +2,34 @@
 
 using namespace std;
 
+#define X first
+#define Y second
+
+int board[502][502];
+bool vis[502][502];
+
+int n, m; //n세로 크기 m가로 크기
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> n >> n; // 행 열
+	
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			cin >> board[i][j];
+		}
+	}
+	
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			cout << board[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+	
 	
 	
 	return 0;
