3426673501792cab5050a3afcd740c3e286d24f4
중화이 <ljh15952@gmail.com>
2023-02-18 11:27:34 +0000

    20:27

---
+++ Workbook/BFS/1926re.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int n, m; //n행 m열
+int board[502][502];
+bool visited[502][502];
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	
+	
+	return 0;
+}
