61764637b12f1198fc1acaf3ff81f00d60d5710e
중화이 <ljh15952@gmail.com>
2023-02-19 13:28:11 +0000

    1028

--- Workbook/BFS/2178.cpp
+++ Workbook/BFS/2178.cpp
@@ -5,11 +5,30 @@ using namespace std;
 #define X second;
 #define Y first;
 
+int N, M;
+string board[105];
+int dist[105][105];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	for(int i = 0; i < N; ++i) cin >> board[i];
+	for(int i = 0; i < N; ++i) fill(dist[i], dist[i]+m, -1);
 	
 	return 0;
 	
 }
+
+/*
+NxM 크기의 배열
+미로 1은 이동할 수 있는 칸
+0은 이동 X, (1,1), 출발 (N,M)의 위치로 이동할 때 지나야 하는 
+최소의 칸 수
+칸을 셀 때 시작 위치와 도착 위치도 포함
+
+
+
+*/
