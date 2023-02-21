7d5b88f70732650411372b18d7cbd359e760804e
중화이 <ljh15952@gmail.com>
2023-02-21 11:24:01 +0000

    2023

---
+++ Workbook/BFS/4179.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "QWE" << '\n';
+	
+	
+	return 0;
+}
+
+/*
+미로, 탈출
+지훈이 위치, 불이 붙은 위치
+불에 타기전에 탈출, 할 수 있는지의 여부, 얼마나 빨리 탈출할 수 있는지
+
+
+*/
