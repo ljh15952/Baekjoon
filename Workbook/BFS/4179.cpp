4137a32dd6ce35a1dd9d9457b179b8ea932dd1b6
중화이 <ljh15952@gmail.com>
2023-02-26 12:47:07 +0000

    4179 solved

--- Workbook/BFS/4179.cpp
+++ Workbook/BFS/4179.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 
-int R, C;
+int R, C; // row행 column열
 
 string board[1002];
 int dist1[1002][1002]; // 불의 전파 시간
@@ -19,12 +19,12 @@ int main(){
 	
 	cin >> R >> C;
 	
-	for(int i = 0; i < C; ++i){
-		fill(dist1[i], dist1[i] + R, -1);
-		fill(dist2[i], dist2[i] + R, -1);
+	for(int i = 0; i < R; ++i){
+		fill(dist1[i], dist1[i] + C, -1);
+		fill(dist2[i], dist2[i] + C, -1);
 	}
 	
-	for(int i = 0; i < C; ++i)
+	for(int i = 0; i < R; ++i)
 		cin >> board[i];	
 	
 	queue<pair<int,int>> Q1;
@@ -42,6 +42,7 @@ int main(){
 		}
 	}
 	
+	
 	// 불 BFS
 	while(!Q1.empty()){
 		auto cur = Q1.front(); Q1.pop();
@@ -50,53 +51,41 @@ int main(){
 			int nx = cur.X + dx[i];
 			int ny = cur.Y + dy[i];
 			
-			if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+			if(nx < 0 || nx >= C || ny < 0 || ny >= R) continue;
 			if(dist1[ny][nx] >= 0 || board[ny][nx] == '#') continue;
 			Q1.push({ny,nx});
 			dist1[ny][nx] = dist1[cur.Y][cur.X] + 1;
 		}
 	}
 	
-	for(int i = 0; i < R; ++i){
-		for(int j = 0; j < C; ++j){
-			cout << dist1[i][j] << ' ';
+	while(!Q2.empty()){
+		auto cur = Q2.front(); Q2.pop();
+		
+		for(int i = 0; i < 4; ++i){
+			int nx = cur.X + dx[i];
+			int ny = cur.Y + dy[i];
+			
+			if(nx < 0 || nx >= C || ny < 0 || ny >= R){
+				cout << dist2[cur.Y][cur.X]+1 << '\n';
+				return 0;
+			}
+			if(dist2[ny][nx] >= 0 || board[ny][nx] == '#') continue;
+			if(dist1[ny][nx] != -1 && dist1[ny][nx] <= dist2[cur.Y][cur.X]+1) continue;
+			dist2[ny][nx] = dist2[cur.Y][cur.X] + 1;
+			Q2.push({ny,nx});
 		}
-		cout << '\n';
 	}
 	
+	cout << "IMPOSSIBLE\n";
 	return 0;
 }
 
 /*
-미로, 탈출
-지훈이 위치, 불이 붙은 위치
-불에 타기전에 탈출, 할 수 있는지의 여부, 얼마나 빨리 탈출할 수 있는지
-
-불은 매 분바다 한칸씩 수평또는 수직으로 이동
-R행, C는 열
-#벽 .지나갈 수 있는 공간 J지훈이의 미로에서의 초기위치 F불이 난 공간
-
-미로 탈출 할 수 없으면 IMPOOSSIBLE 출력
-탈출하면 가장 빠른 탈출 시간 출력
-
-####
-#JF#
-#..#
-#..# 
-
-####
-#FF#
-#JF#
-#..#
-
-####
-#FF#
-#FF#
-#JF#
-
-####
-#FF#
-#FF#
-#FF#  탈출
- J
+5 6
+#####
+#J.F#
+#...#
+#...#
+#...#
+#...#
 */
