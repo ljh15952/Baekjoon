b0eec821b37ccc49e5b69058f79022c75c01594b
중화이 <ljh15952@gmail.com>
2023-03-10 13:10:04 +0000

    5014 solved

--- Workbook/BFS/5014.cpp
+++ Workbook/BFS/5014.cpp
@@ -1,10 +1,45 @@
-#include <iostream> 
+#include <bits/stdc++.h> 
 
 using namespace std;
+
+int F, S, G, U, D;
+
+int board[1000002];
 
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cin >> F >> S >> G >> U >> D;
+	
+	fill(board+1, board+F+1, -1);
+	queue<int> Q;
+	
+	Q.push(S);
+	board[S] = 0;
+	
+	int dx[2] = {U, -D};
+	
+	while(!Q.empty()){
+		auto cur = Q.front(); Q.pop();
+		
+		for(int i = 0; i < 2; ++i){
+			int nx = cur + dx[i];
+			
+			if(nx <= 0 || nx > F || board[nx] != -1) continue;
+			
+			board[nx] = board[cur] + 1;
+			Q.push(nx);
+		}
+	}
+	
+	if(board[G] == -1) 	cout << "use the stairs\n";
+	else cout << board[G] << '\n';
+	
+	
+	// for(int i = 1; i <= F; ++i){
+	// 	cout << i << "층 " << board[i] << '\n';
+	// }
 	
 	return 0;
 }
@@ -20,6 +55,11 @@ U버튼은 위로 U층을 가는 버튼, D버튼은 아래로 D층을 가는 버
 만약 U층 위, D층 아래에 해당하는 층이 없을 때는 엘리베이터 움직이지 않음
 
 강호가 G층에 도착하려면 버튼을 적어도 몇 번 눌러야 하는지 구하는 프로그램
+엘리베이터로 이동할 수 없을 때는 use the stairs를 출력한다.
 
+F, S, G, U, D 
+10 1 10 2 1
+1 -> 3 -> 5 -> 7 -> 9 -> 8 -> 10
+6번
 
 */
