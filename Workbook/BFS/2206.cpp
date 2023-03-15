3767fe6345302184020744c6bee6c02d6d5432dc
중화이 <ljh15952@gmail.com>
2023-03-15 13:46:56 +0000

    2246

--- Workbook/BFS/2206.cpp
+++ Workbook/BFS/2206.cpp
@@ -2,10 +2,52 @@
 
 using namespace std;
 
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int board[1001][1001];
+int dist[1001][1001][2];
+// dist[y][x][0] 벽을 하나도 부수지 않고 (x,y)까지 오는데 걸리는 비용
+// dist[y][x][1] 벽을 하나만 부수고 (x,y)까지 오는데 걸리는 비용, (x,y)가 벽이라
+// 부수는 경우도 포함
+
+int N, M;
+
+bool OOB(int y, int x){
+	return (x < 0 || x >= M || y < 0 || y >= N)
+}
+
+int bfs(){
+	for(int i = 0; i < N; ++i)
+		for(int j = 0; j < M; ++j)
+			dist[i][j][0] = dist[i][j][1] = -1;
+	
+	
+	
+}
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> N >> M;
+	
+	for(int i = 0; i < N; ++i){
+		for(int j = 0; j < M; ++j){
+			cin >> board[N][M];
+		}
+	}
+	
+	Q.push({0,0});
+	
+	while(!Q.empty()){
+		auto cur = Q.front(); Q.pop();
+		
+		
+		
+	}
+	
+	
+	// if(ny == N-1 && nx == M-1) 끝
 	
 	return 0;
 }
@@ -22,5 +64,8 @@ NxM 행렬로 표현되는 맵이 있다.
 벽을 한 개 까지 부수고 이동하여도 된다.
 
 한 칸에서 이동할 수 있는 칸은 상하좌우로 인접한 칸
-
+불가능할 때는 -1 출력
+(1,1) (N,M)은 항상 0
+N 행
+M 열 
 */
