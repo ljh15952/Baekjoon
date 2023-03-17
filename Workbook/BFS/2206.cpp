68867611ba1d5474fe106c9643be258f66b9e42c
중화이 <ljh15952@gmail.com>
2023-03-17 13:51:08 +0000

    too hard..

--- Workbook/BFS/2206.cpp
+++ Workbook/BFS/2206.cpp
@@ -9,7 +9,7 @@ int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
 char board[1001][1001];
-int dist[1001][1001];
+int dist[1001][1001][2];
 
 int N, M;
 
@@ -20,26 +20,27 @@ bool OOB(int y, int x){
 int bfs(){
 	for(int i = 0; i < N; ++i)
 		for(int j = 0; j < M; ++j)
-			dist[i][j] = -1;
+			dist[i][j][0] = -1;
 	
-	dist[0][0] = 1;
+	dist[0][0][0] = 1;
 	
-	queue<pair<int,int>> q;
-	q.push({0,0});
+	queue<tuple<int,int,int>> q;
+	q.push({0,0,0});
 	while(!q.empty()){
-		auto cur = q.front();
+		int x,y,broken;
+		tie(y,x,broken) = q.front();
 		q.pop();
 		
 		for(int i = 0; i < 4; ++i){
-			int nx = cur.X + dx[i];
-			int ny = cur.Y + dy[i];
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 			
 			if(OOB(ny,nx)) continue;
-			if(dist[ny][nx] > 0) continue;
+			if(dist[ny][nx][0] > 0) continue;
 			if(board[ny][nx] == '1') continue;
 
-			dist[ny][nx] = dist[cur.Y][cur.X] + 1;
-			q.push({ny,nx});
+			dist[ny][nx][0] = dist[x][y][0] + 1;
+			q.push({ny,nx,0});
 		}
 		
 	}
@@ -59,7 +60,7 @@ int main(){
 	
 	for(int i = 0; i < N; ++i){
 		for(int j = 0; j < M; ++j){
-			cout << dist[i][j] << ' ';
+			cout << dist[i][j][0] << ' ';
 		}	
 		cout << '\n';
 	}
