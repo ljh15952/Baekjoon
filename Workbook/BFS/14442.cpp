158b62663c983614717e56d2d5343e28b7a709f1
중화이 <ljh15952@gmail.com>
2023-04-01 07:11:04 +0000

    14442 solving

--- Workbook/BFS/14442.cpp
+++ Workbook/BFS/14442.cpp
@@ -2,13 +2,73 @@
 
 using namespace std;
 
+#define first Y
+#define second X
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int N, M, K;
+
+char board[1001][1001];
+
+int dist[2][1001][1001];
+
+queue<tuple<int,int,int>> Q;
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> N >> M >> K;
+	
+	for(int i = 0; i < N; ++i){
+		for(int j = 0; j < M; ++j){
+			cin >> board[i][j];
+		}
+	}
+	
+	Q.push({0,0,0});
+	dist[0][0][0] = 1;
+	dist[1][0][0] = 1;
+	
+	while(!Q.empty()){
+		int x,y,z;
+		tie(z,y,x) = Q.front(); Q.pop();
+		
+		for(int i = 0; i < 4; ++i){
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+			if(dist[z][ny][nx] > 0) continue;
+			if(board[ny][nx] == '1') continue;
+			
+			dist[z][ny][nx] = dist[z][y][x] + 1;
+			Q.push({z,ny,nx});
+		}
+	}
+	
+	for(int i = 0; i < N; ++i){
+		for(int j = 0; j < M; ++j){
+			cout << dist[0][i][j] << ' ';
+		}
+		cout << '\n';
+	}
 	
 	return 0;
 }
+
+
+/*
+6 4 1
+0000
+1110
+1000
+0000
+0111
+0000
+
+*/
 
 /*
 NxM의 행렬로 표현되는 맵
