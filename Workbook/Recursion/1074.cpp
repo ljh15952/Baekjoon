58dedc0c43871c63d037540b80aea9962daae8cf
중화이 <ljh15952@gmail.com>
2023-04-16 13:58:06 +0000

    1074

--- Workbook/Recursion/1074.cpp
+++ Workbook/Recursion/1074.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
+int N;
+int r, c; // row행 col열
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
-	
+	cin >> N >> r >> c;
+		
 	return 0;
 }
 // z자 탐색 2차원 배열
