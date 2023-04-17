e1c7dd72c2dbd335a98109a3db3b779598f8e96b
중화이 <ljh15952@gmail.com>
2023-04-17 13:18:48 +0000

    so hard

--- Workbook/Recursion/1074.cpp
+++ Workbook/Recursion/1074.cpp
@@ -6,17 +6,15 @@ int N;
 int R, C; // row행 col열
 int M;
 
-void func(int r, int c){
-	if(r == M - 1){
-		
-	}
-	if(c == M - 1){
-		
-	}
+int func(int n, int r, int c){
+	if(n == 0) return 0;
 	
-	cout << r << ' ' << c << '\n';
+	int half = 1 << (n-1);
 	
-	
+	if(r < half && c < half) return func(n-1, r, c);
+	if(r < half && c <= half) return half*half + func(n-1, r, c-half);
+	if(r >= half && c < half) return 2*half*half + func(n-1, r-half, c);
+	return 3*half*half + func(n-1, r-half, c-half);
 }
 
 int main(){
@@ -25,11 +23,7 @@ int main(){
 	
 	cin >> N >> R >> C;
 	
-	M = pow(2,N);
-	cout << M;
-	
-	
-	
+	cout << func(N, R, C) << '\n';
 	
 	return 0;
 }
