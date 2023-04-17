5726d240627dab4a596130df3e02dfe660930cc5
중화이 <ljh15952@gmail.com>
2023-04-17 13:21:33 +0000

    1074

--- Workbook/Recursion/1074.cpp
+++ Workbook/Recursion/1074.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int N;
 int R, C; // row행 col열
-int M;
 
 int func(int n, int r, int c){
 	if(n == 0) return 0;
@@ -12,7 +11,7 @@ int func(int n, int r, int c){
 	int half = 1 << (n-1);
 	
 	if(r < half && c < half) return func(n-1, r, c);
-	if(r < half && c <= half) return half*half + func(n-1, r, c-half);
+	if(r < half && c >= half) return half*half + func(n-1, r, c-half);
 	if(r >= half && c < half) return 2*half*half + func(n-1, r-half, c);
 	return 3*half*half + func(n-1, r-half, c-half);
 }
