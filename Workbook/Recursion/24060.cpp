e0b10450863e6ff9543a05697d894b79b63899bf
중화이 <ljh15952@gmail.com>
2023-04-25 14:27:08 +0000

    24060

--- Workbook/Recursion/24060.cpp
+++ Workbook/Recursion/24060.cpp
@@ -2,17 +2,57 @@
 
 using namespace std;
 
-void merge_sort(int arr[]){
-	
-}
+int A, K, cnt;
+int arr[500000];
+int tmp[500000];
+int ans;
 
+void merge_sort(int left, int right){
+	if(left < right){
+		int mid = (left + right) / 2;
+		merge_sort(left, mid);
+		merge_sort(mid + 1, right);
+		
+		if(cnt >= K) return;
+		
+		int i = left;
+		int j = mid + 1;
+		int t = left;
+		while(i <= mid && j <= right){
+			if(arr[i] <= arr[j])
+				tmp[t++] = arr[i++];
+			else
+				tmp[t++] = arr[j++];
+		}
+		while(i <= mid)
+			tmp[t++] = arr[i++];
+		while(j <= right)
+			tmp[t++] = arr[j++];
+
+		for(i = left; i <= right; ++i){
+			arr[i] = tmp[i];
+			if(++cnt == K){
+				ans = arr[i];
+				break;
+			}
+		}
+	}
+}
 
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> A >> K;	
 	
+	for(int i = 0; i < A; ++i){
+		cin >> arr[i];
+	}
+	
+	merge_sort(0, A-1);
+	
+	if(cnt < K) cout << -1 << '\n';
+	else cout << ans << '\n';
 	
 	return 0;
 }
