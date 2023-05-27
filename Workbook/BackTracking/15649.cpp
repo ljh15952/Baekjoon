9797dddac3f82c5799478b046069b7bf449d3a0e
중화이 <ljh15952@gmail.com>
2023-05-27 09:57:15 +0000

    15649

---
+++ Workbook/BackTracking/15649.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "!#" << '\n';
+	
+	return 0;
+	
+}
+
+/*
+N과 M 
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인
+수열을 모두 구하는 프로그램
+* 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+
+
+*/
