0d8621efa5d08aaee0492910233f5ddca0e91d25
중화이 <ljh15952@gmail.com>
2023-05-27 13:17:35 +0000

    15649

--- Workbook/BackTracking/15649.cpp
+++ Workbook/BackTracking/15649.cpp
@@ -2,10 +2,34 @@
 
 using namespace std;
 
+int N, M;
+int arr[10];
+bool isused[10];
+
+void func(int k){
+	if(k == M){
+		for(int i = 0; i < M; ++i)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	
+	for(int i = 1; i <= N; ++i){
+		if(!isused[i]){
+			arr[k] = i;
+			isused[i] = 1;
+			func(k+1);
+			isused[i] = 0;
+		}
+	}
+}
+
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "!#" << '\n';
+	cin >> N >> M;
+	func(0);
 	
 	return 0;
 	
@@ -17,5 +41,22 @@ N과 M
 수열을 모두 구하는 프로그램
 * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 
+3 1
+1부터 3까지 자연수 중에서 중복 없이 1개를 고른 수열 
+1, 2, 3
+4 2
+1부터 4까지 자연수 중에서 중복없이 2개를 고른 수열
+1 2
+1 3
+1 4
+2 1
+2 3
+2 4
+3 1
+3 2
+3 4
+4 1
+4 2
+4 3
 
 */
