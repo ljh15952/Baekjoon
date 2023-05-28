4957e2b4f4bedc6f003a8388d68644cea0c55053
중화이 <ljh15952@gmail.com>
2023-05-28 14:13:43 +0000

    N-Queen

---
+++ Workbook/BackTracking/9663.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "9633" << '\n';
+	
+	return 0;
+}
+
+
+/*
+N-Queen
+
+NxN 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제
+퀸을 놓는 방법의 수를 구해라 
+(1<=N<=15)
+
+
+*/
