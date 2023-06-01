a1624b89208bc08c1342c6d972589e6edc9f960d
중화이 <ljh15952@gmail.com>
2023-06-01 13:56:25 +0000

    9663

--- Workbook/BackTracking/9663.cpp
+++ Workbook/BackTracking/9663.cpp
@@ -4,30 +4,41 @@ using namespace std;
 
 int N;
 
-int queenMove[][];
+bool isused1[40];
+bool isused2[40];
+bool isused3[40];
+
+int cnt = 0;
+
+void func(int cur){
+	
+	if(cur == N){
+		cnt++;
+		return;
+	}
+	
+	for(int i = 0; i < N; ++i){
+		if(isused1[i] || isused2[i + cur] || isused3[cur-i+N-1])
+			continue;
+		
+		isused1[i] = 1;
+		isused2[i + cur] = 1;
+		isused3[cur - i + N - 1] = 1;
+		func(cur + 1);
+		isused1[i] = 0;
+		isused2[i + cur] = 0;
+		isused3[cur - i + N - 1] = 0;
+	}
+	
+}
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cin << N;
+	cin >> N;
+	func(0);
+	
+	cout << cnt << '\n';
 	
 	return 0;
 }
-
-
-/*
-N-Queen
-
-NxN 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제
-퀸을 놓는 방법의 수를 구해라 
-(1<=N<=15)
-
-1. board[0][0]
-2. board[0][0] 퀸의 움직임 만큼 배제
-3. 다음 가능한 곳 놓기 board[1][1]
-4. board[1][1] 퀸의 움직임 만큼 배제
-5. 다음 가능한 곳 놓기
-..
-다 놨으면
-대충 감오는데
-*/
