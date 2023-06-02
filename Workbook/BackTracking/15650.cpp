2e0ee8f0e7f03edbaf24fc3610cb227649bd176c
중화이 <ljh15952@gmail.com>
2023-06-02 12:52:50 +0000

    15650

---
+++ Workbook/BackTracking/15650.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "QWE" << '\n';
+	
+	return 0;
+}
+
+/*
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램
+1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+
+3 1
+1
+2
+3
+
+4 2
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+
+4 4
+1 2 3 4
+
+*/
