cc0c60c390c225976c55d0e67b29fd7e15913852
중화이 <ljh15952@gmail.com>
2023-06-05 15:06:53 +0000

    15657 solved

--- Workbook/BackTracking/15657.cpp
+++ Workbook/BackTracking/15657.cpp
@@ -2,11 +2,40 @@
 
 using namespace std;
 
-int main(){
+int N, M;
+int num[10];
+int arr[10];
+
+void func(int k){
 	
+	if(k == M){
+		for(int i = 0; i < M; ++i)
+			cout << num[arr[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+	
+	int st = 0;
+	if(k != 0) st = arr[k-1];
+	for(int i = st; i < N; ++i){
+		arr[k] = i;
+		func(k+1);
+	}
+	
+	
+}
+
+int main(){
+
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> N >> M;
+	for(int i = 0; i < N; ++i)
+		cin >> num[i];
+	
+	sort(num, num+N);
+	
+	func(0);
 	
 	return 0;
 }
