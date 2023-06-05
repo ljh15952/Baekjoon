d3ad0b969ad7baf80c58832e65057654ea5b0abb
중화이 <ljh15952@gmail.com>
2023-06-05 14:55:45 +0000

    15657

---
+++ Workbook/BackTracking/15657.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "QWE" << '\n';
+	
+	return 0;
+}
+
+/*
+같은 수를 여러 번 골라도 됨
+비내림차순
+
+*/
