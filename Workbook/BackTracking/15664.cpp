69430ebc71bb5fe6a21d5e6d313051400b0e7ae6
중화이 <ljh15952@gmail.com>
2023-06-06 12:46:40 +0000

    15664 solved

--- Workbook/BackTracking/15664.cpp
+++ Workbook/BackTracking/15664.cpp
@@ -2,11 +2,50 @@
 
 using namespace std;
 
+int N,M;
+int num[10];
+int arr[10];
+int isused[10];
+
+void func(int k){
+	
+	if(k == M){
+		for(int i = 0; i < M; ++i)
+			cout << num[arr[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+	
+	int tmp = 0;
+	int st = 0;
+	if(k != 0) st = arr[k-1];
+	for(int i = st; i < N; ++i){
+		if(!isused[i] && tmp != num[i]){
+			arr[k] = i;
+			isused[i] = 1;
+			tmp = num[i];
+			func(k+1);
+			isused[i] = 0;
+		}
+		
+	}
+	
+}
+
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "WQE" << '\n';
+	cin >> N >> M;
+	
+	for(int i = 0; i < N; ++i){
+		cin >> num[i];
+	}
+	
+	sort(num, num+N);
+	
+	func(0);
 	
 	return 0;
 }
