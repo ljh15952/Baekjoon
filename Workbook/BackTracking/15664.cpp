5e375cc87ff8b59736c4ce9900629ca32a89bff4
중화이 <ljh15952@gmail.com>
2023-06-06 01:48:16 +0000

    15664

---
+++ Workbook/BackTracking/15664.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "WQE" << '\n';
+	
+	return 0;
+}
+
+/*
+N과 M
+비내림차순 중복X
+
+
+*/
