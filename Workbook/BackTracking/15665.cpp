66bbcf984ccab5f93d6fa050b9f427da381f84d2
중화이 <ljh15952@gmail.com>
2023-06-06 15:43:20 +0000

    15665

---
+++ Workbook/BackTracking/15665.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+int arr[10];
+int num[10];
+int isused[10];
+
+void func(int k){
+	
+}
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; ++i)
+		cin >> num[i];
+	
+	sort(num, num+n);
+	
+	func(0);
+	
+	return 0;
+	
+}
