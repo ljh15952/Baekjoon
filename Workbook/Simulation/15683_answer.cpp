42fc3b6db793ea92e7eb86784f3153a454228ff5
LeeJeungHwa <ljh15952@gmail.com>
2023-07-05 22:09:09 +0900

    Get quaternary

--- Workbook/Simulation/15683_answer.cpp
+++ Workbook/Simulation/15683_answer.cpp
@@ -1,1 +1,24 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	
+	// 4진수
+	for(int tmp = 0; tmp < 64; tmp++){
+	    int brute = tmp;
+	    cout << tmp << " : ";
+	    for(int i = 0; i < 3; i++){
+	        cout << brute % 4;
+	        brute /= 4;
+	    }
+	    cout << '\n';
+	}
+    
+	
+	return 0;
+}
