61a9fb236f78a66e51504aa4cc41b8288fbca0dd
중화이 <ljh15952@gmail.com>
2023-07-22 07:56:43 +0000

    14499 start

---
+++ Workbook/Simulation/14499.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cout << "QWR" << '\n';
+	
+	return 0;
+}
+
+/*
+14499
+
+주사위 굴리기
+
+
+*/
