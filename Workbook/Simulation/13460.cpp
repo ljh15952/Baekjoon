b92fa11768f4540265ac66d64ab337694b2fe99d
중화이 <ljh15952@gmail.com>
2023-09-10 07:24:46 +0000

    13460

--- Workbook/Simulation/13460.cpp
+++ Workbook/Simulation/13460.cpp
@@ -2,81 +2,103 @@
 
 using namespace std;
 
+#define Y second
+#define X first
+
 int n, m;
-char board[13][13];
+string board[11];
+pair<int, int> red, blue; // 빨간 구슬과 파란구슬의 위치
 
-void print(){
-	cout << '\n';
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cout << board[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-}
+// dist[a][b][c][d]
+// 빨간 구슬 (a,b) 파란 구슬 (c,d)에 위치한 상황에 도달하기 위한 동작의 횟수
+int dist[11][11][11][11]; 
 
-int ry, rx; // 빨간 구슬
-int by, bx; // 파란 구슬
-int hy, hx; // 구멍
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
 
-void backTracking(){
+
+int bfs() {
+  	queue<tuple<int,int,int,int>> q;
+	q.push({red.X, red.Y, blue.X, blue.Y});
+	dist[red.X][red.Y][blue.X][blue.Y] = 0;
 	
+	while(!q.empty()){
+		int rx, ry, bx, by;
+		tie(rx, ry, bx, by) = q.front();
+		q.pop();
+		
+		int cnt = dist[rx][ry][bx][by];
+		
+		if(cnt >= 10)
+			return -1;
+		// 4방향으로 기울이기
+		for(int i = 0; i < 4; i++){
+			int n_rx = rx, n_ry = ry, n_bx = bx, n_by = by;
+
+			while(board[n_bx + dx[i]][n_by + dy[i]] == '.'){
+				n_bx += dx[i];
+				n_by += dy[i];
+			}
+
+
+		if(board[n_bx + dx[i]][n_by + dy[i]] == 'O') continue;
+
+		while(board[n_rx + dx[i]][n_ry + dy[i]] == '.'){
+			n_rx += dx[i];
+			n_ry += dy[i];
+		}
+
+		if(board[n_rx + dx[i]][n_ry + dy[i]] == 'O') return cnt + 1;
+
+		  // Red, Blue가 겹쳐진 경우 늦게 출발한 구슬을 한칸 뒤로 이동
+		if((n_rx == n_bx) && (n_ry == n_by)){
+			if(i == 0)
+				ry < by ? n_ry-- : n_by--;
+			else if(i == 1)
+				rx < bx ? n_rx-- : n_bx--;
+			else if(i == 2)
+				ry > by ? n_ry++ : n_by++;
+			else
+				rx > bx ? n_rx++ : n_bx++;
+		}
+			if(dist[n_rx][n_ry][n_bx][n_by] != -1) continue;
+			dist[n_rx][n_ry][n_bx][n_by] = cnt + 1;
+			q.push({n_rx, n_ry, n_bx, n_by});
+		}
+	}
+	return -1;
 }
 
 
 int main(){
 	
 	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	// 거리 배열 초기화
+	for(int i = 0; i < 10; i++)
+		for(int j = 0; j < 10; j++)
+			for(int k = 0; k < 10; k++)
+				fill(dist[i][j][k], dist[i][j][k]+10, -1);
 	
 	cin >> n >> m;
 	
 	for(int i = 0; i < n; i++){
+		cin >> board[i];
 		for(int j = 0; j < m; j++){
-			cin >> board[i][j];
+			
+			if(board[i][j] == 'R'){
+				red = {i, j};
+				board[i][j] = '.';
+			}else if(board[i][j] == 'B'){
+				blue = {i , j};
+				board[i][j] = '.';
+			}
 		}
 	}
 
-	print();
-	
-	backTracking();
+	cout << bfs() << '\n';
 	
 	return 0;
 }
-
-/*
-계획
-
-백트래킹을 해야함
-현재 R이 움직일 수 있는 위치 판별
-움직여 보고->R이 구멍빠지면 끝(바로 끝내면 안됨 B움직여보고), B가 구멍 빠지면 안되는 것
-R이 지나간 길에 표시
-
-
-*/
-
-/*
-구슬 탈출
-
-직사각형 보드에 빨간 구슬과 파란 구슬을 하나씩 넣은 다음, 빨간 구슬을 구멍을 통해 빼내는 게임
-
-보드의 세로크기 N, 가로 크기 M
-보드에는 구멍이 하나 있다.
-
-빨간 구슬과 파란 구슬은 각각 하나씩 들어가 있다. 
-파란 구슬이 구멍에 들어가면 안된다.
-
-기울이기를 통해 구슬을 움직인다.
-빨간 구슬과 파란 구슬 동시에 구멍에 빠져도 실패
-
-기울이는 동작을 그만하는 것은 더 이상 구슬이 움직이지 않을 때 까지 
-
-보드의 상태가 주어졌을 때, 최소 몇 번 만에 빨간 구슬을 구멍을 통해 빼낼 수 있는지 구하라
-
-. 빈칸
-# 벽
-O 구멍
-R 빨간 구슬
-B 파란 구슬
-
-*/
