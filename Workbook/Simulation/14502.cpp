e1810f92cb35c499b7a668d70ce212a13fc8e32b
중화이 <ljh15952@gmail.com>
2023-09-16 12:31:11 +0000

    14502

--- Workbook/Simulation/14502.cpp
+++ Workbook/Simulation/14502.cpp
@@ -2,11 +2,29 @@
 
 using namespace std;
 
+int n, m;
+int board[9][9];
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cin >> board[i][j];
+		}
+	}
+	
+	
+	cout << '\n';
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cout << board[i][j] << ' ';
+		}
+		cout << '\n';
+	}
 	
 	return 0;
 }
