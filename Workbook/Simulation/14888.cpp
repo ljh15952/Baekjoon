56282631a37db5fe99731b544db0dd33787d0ee1
중화이 <ljh15952@gmail.com>
2023-10-09 09:25:45 +0000

    14888

--- Workbook/Simulation/14888.cpp
+++ Workbook/Simulation/14888.cpp
@@ -2,17 +2,42 @@
 
 using namespace std;
 
+int N;
+int arr[15];
+int oper[4];
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "QWE" << '\n';
+	cin >> N;
+	
+	for(int i = 0; i < N; i++){
+		cin >> arr[i];
+	}
+	
+	for(int i = 0; i < 4; i++){
+		cin >> oper[i];
+	}
+	
+	for(int i = 0; i < N; i++){
+		cout << arr[i] << '\n';
+	}
+	
+	for(int i = 0; i < 4; i++){
+		cout << oper[i] << '\n';
+	}
+	
 	
 	return 0;
 }
 
 /*
 연산자 끼워넣기
+
+6개의 수
+연산자 + 2, - 1, x 1, / 1 5개
+가능한 경우의 수 60
 
 
 */
