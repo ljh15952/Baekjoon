f03e1f3c18fae4654e7524b660d6e02ff988888b
중화이 <ljh15952@gmail.com>
2023-10-30 04:27:30 +0000

    Insert sort

--- src/Uni/Uni1.cpp
+++ src/Uni/Uni1.cpp
@@ -6,7 +6,26 @@ int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cout << "UNI" << '\n';
+	int a[10] = {1,2,3,4,11,6,7,8,9,10};
+	
+	int n = 10;
+	
+	for(int i = 1; i < n; i++){
+		
+		int j = i - 1;
+		int v = a[i];
+		
+		while(j >= 0 && v < a[j]){
+			a[j+1] = a[j];
+			j--;
+		}
+		
+		a[j+1] = v;
+	}
+	
+	for(int i = 0; i < n; i++){
+		cout << a[i] << ' ';
+	}
 	
 	return 0;
 }
