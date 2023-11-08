1732d6e56b846b80f53a177fb1b5c8fb6ac1545e
중화이 <ljh15952@gmail.com>
2023-11-08 07:32:59 +0000

    14890 solved

--- Workbook/Simulation/14890.cpp
+++ Workbook/Simulation/14890.cpp
@@ -4,21 +4,56 @@ using namespace std;
 
 int N, L;
 int board[101][101];
-
-bool canSetHighStair(int k){
+bool isStair[101];
+int ans = 0;
+bool canSetHighStair(int r, int k){
 	for(int i = 0; i < L; i++){
-		if(board[k] != board[k - i]){				
+		if(board[r][k] != board[r][k - i]){				
 			return false;
 		}
+		if(isStair[k - i]){
+			return false;
+		}
+		isStair[k - i] = true;
 	}
 	return true;
 }
 
-bool canSetLowStair(int k){
+bool canSetLowStair(int r, int k){
 	for(int i = 1; i <= L; i++){
-		if(board[k] != board[k + i]){				
+		if(board[r][k] - 1 != board[r][k + i]){				
 			return false;
 		}
+		if(isStair[k + i]){
+			return false;
+		}
+		isStair[k + i] = true;
+	}
+	return true;
+}
+
+bool canSetHighStair2(int r, int k){
+	for(int i = 0; i < L; i++){
+		if(board[r][k] != board[r - i][k]){				
+			return false;
+		}
+		if(isStair[r - i]){
+			return false;
+		}
+		isStair[r - i] = true;
+	}
+	return true;
+}
+
+bool canSetLowStair2(int r, int k){
+	for(int i = 1; i <= L; i++){
+		if(board[r][k] - 1 != board[r + i][k]){				
+			return false;
+		}
+		if(isStair[r + i]){
+			return false;
+		}
+		isStair[r + i] = true;
 	}
 	return true;
 }
@@ -36,27 +71,58 @@ int main(){
 	}
 	
 	for(int i = 0; i < N; i++){
+		
+		for(int k = 0; k < N; k++){
+			isStair[k] = false;
+		}
+		
 		for(int j = 0; j < N; j++){
 			
 			if(j == N-1){
-				cout << "Can go" << '\n';
+				ans++;
 				break;
 			}
-
-			if(board[j] == board[j+1] + 1){
-				if(!canSetHighStair(j)){
+			
+			if(board[i][j] + 1 == board[i][j+1]){
+				if(!canSetHighStair(i, j)){
 					break;
 				}
-			}else if(board[j] == board[j+1] - 1){
-				if(!canSetLowStair(j)){
+			}else if(board[i][j] - 1 == board[i][j+1]){
+				if(!canSetLowStair(i, j)){
 					break;
 				}
-			}else if(board[j] != board[j+1]){
+			}else if(board[i][j] != board[i][j+1]){
 				break;
 			}
 		}
 	}
 	
+	for(int j = 0; j < N; j++){
+		
+		for(int k = 0; k < N; k++){
+			isStair[k] = false;
+		}
+		for(int i = 0; i < N; i++){
+			
+			if(i == N-1){
+				ans++;
+				break;
+			}
+			if(board[i][j] + 1 == board[i+1][j]){
+				if(!canSetHighStair2(i ,j)){
+					break;
+				}
+			}else if(board[i][j] - 1 == board[i+1][j]){
+				if(!canSetLowStair2(i, j)){
+					break;
+				}
+			}else if(board[i][j] != board[i+1][j]){
+				break;
+			}
+		}
+	}
+	
+	cout << ans << '\n';
 
 	
 	return 0;
