6ec75cb77452206d3262d5bf22d73f34df820366
중화이 <ljh15952@gmail.com>
2023-11-08 06:00:21 +0000

    14890

--- Workbook/Simulation/14890.cpp
+++ Workbook/Simulation/14890.cpp
@@ -5,6 +5,24 @@ using namespace std;
 int N, L;
 int board[101][101];
 
+bool canSetHighStair(int k){
+	for(int i = 0; i < L; i++){
+		if(board[k] != board[k - i]){				
+			return false;
+		}
+	}
+	return true;
+}
+
+bool canSetLowStair(int k){
+	for(int i = 1; i <= L; i++){
+		if(board[k] != board[k + i]){				
+			return false;
+		}
+	}
+	return true;
+}
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
@@ -17,6 +35,27 @@ int main(){
 		}
 	}
 	
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			
+			if(j == N-1){
+				cout << "Can go" << '\n';
+				break;
+			}
+
+			if(board[j] == board[j+1] + 1){
+				if(!canSetHighStair(j)){
+					break;
+				}
+			}else if(board[j] == board[j+1] - 1){
+				if(!canSetLowStair(j)){
+					break;
+				}
+			}else if(board[j] != board[j+1]){
+				break;
+			}
+		}
+	}
 	
 
 	
@@ -26,8 +65,6 @@ int main(){
 /*
 
 1. 가로 for문, 세로 for문 
-2. 시작할 때 원본배열 복사하여 새로운 배열 생성
-3. 선형적으로 진행
 4. 
 case1 현재 i (값 v) 보다 i+1 위치한 수가 클(v+1) 경우
 만약 L이 2라면 i, i-1이 v일 경우 can go
