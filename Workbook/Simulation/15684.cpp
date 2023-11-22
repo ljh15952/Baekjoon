32646b85d572f95b15c8632fdbca779f72d5bd64
중화이 <ljh15952@gmail.com>
2023-11-22 05:22:14 +0000

    15684

--- Workbook/Simulation/15684.cpp
+++ Workbook/Simulation/15684.cpp
@@ -2,19 +2,73 @@
 
 using namespace std;
 
-int N, M, H;
-int arr[15][35];
+#define X first
+#define Y second
+
+bool ladder[32][12];
+int idxs[3];
+
+vector<pair<int, int>> coords;
+
+int n, m, h;
+
+bool check(){
+	
+	for(int j = 1; j <= n; j++){
+		int cur = j;
+		for(int i = 1; i <= h; i++){
+			if(ladder[i][cur - 1]) cur--;
+			else if(ladder[i][cur]) cur++;
+		}
+		if(cur != j) return false;
+	}
+	return true;
+}
+
 int main(){
 	
 	ios::sync_with_stdio(0); cin.tie(0);
 	
-	cin >> N >> M >> H;
+	cin >> n >> m >> h;
 	
-	for(int i = 0; i < M; i++){
+	while(m--){
 		int a, b;
 		cin >> a >> b;
-		arr[a][b] = 1;
+		ladder[a][b] = true;
 	}
+	
+	for(int i = 1; i <= h; i++){
+		for(int j = 1; j < n; j++){
+			if(ladder[i][j - 1] || ladder[i][j] || ladder[i][j + 1]) continue;
+			coords.push_back({i, j});
+		}
+	}
+	
+	if(check()){
+		cout << 0;
+		return 0;
+	}
+	
+	int ans = 0x7f7f7f7f;
+	int sz = coords.size();
+	
+	for(int i = 0; i < sz; i++){
+		ladder[coords[i].X][coords[i].Y] = true;
+		if(check()) ans = min(ans, 1);
+		for(int j = i+1; j < sz; j++){
+			ladder[coords[j].X][coords[j].Y] = true;
+			if(check()) ans = min(ans, 2);
+		  	for(int k = j+1; k < sz; k++){
+				ladder[coords[k].X][coords[k].Y] = true;
+				if(check()) ans = min(ans, 3);
+				ladder[coords[k].X][coords[k].Y] = false;
+		  	}
+			ladder[coords[j].X][coords[j].Y] = false;
+		}
+		ladder[coords[i].X][coords[i].Y] = false;
+	}
+	if(ans == 0x7f7f7f7f) ans = -1;
+	cout << ans;
 	
 	return 0;
 }
