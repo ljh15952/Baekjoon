9c734365647ac2d15bdae2a42ceadc73aad3ba9e
중화이 <ljh15952@gmail.com>
2023-12-04 06:00:21 +0000

    Permute

--- Sangorithm/BackTracking/Permute.cpp
+++ Sangorithm/BackTracking/Permute.cpp
@@ -2,9 +2,48 @@
 
 using namespace std;
 
+int N = 3;
+int arr[3];
+
+
+bool promising(int k, int i){
+	bool flag = true;
+	
+	int j = 0;
+	while(j < k && flag){
+		if(i == arr[j])
+			flag = false;
+		j++;
+	}
+	
+	return flag;
+}
+void perm(int k){
+	
+	if(k == N){
+		for(int i = 0; i < N; i++)
+			cout << arr[i] << ' ';
+			cout << '\n';
+			return;
+	}
+	
+	for(int i = 1; i <= N; i++){
+		if(promising(k, i)){
+			arr[k] = i;
+			perm(k+1);
+		}
+	}
+}
+
 int main(){
 	
-	cout << "QWE" << '\n';
+	perm(0);
 	
 	return 0;
 }
+
+/*
+
+1,2,3,,N 의 모든 순열을 생성한 후 출력
+
+*/
