5bad873ef7e03cd5985ec3ea78a91256f5764bcf
중화이 <ljh15952@gmail.com>
2024-04-16 06:46:19 +0000

    test

---
+++ Workbook/Sort/11728_re.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A[1000001];
+int B[1000001];
+
+int N, M;
+
+int main(){
+	
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	cin >> N >> M;
+	
+	for(int i = 0; i < N; i++){
+		cin >> A[i];
+	}
+	
+	for(int i = 0; i < M; i++){
+		cin >> B[i];	
+	}
+	
+	for(int i = 0; i < N; i++){
+		cout << A[i] << ' ';
+	}
+	cout << '\n';
+	
+	for(int i = 0; i < M; i++){
+		cout << B[i] << ' ';
+	}
+	cout << '\n';
+	
+	
+	
+	
+	return 0;
+}
