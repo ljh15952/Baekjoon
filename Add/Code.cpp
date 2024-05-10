f1cf9c675b5d77e7c1aacb1ecbbcc0bc1f29008f
LeeJeungHwa <ljh15952@gmail.com>
2024-05-10 19:23:28 +0900

    Bubble sort

--- Add/Code.cpp
+++ Add/Code.cpp
@@ -10,23 +10,17 @@ int main()
 	int arr[10] = { 3, 2, 7, 116, 62, 235, 1, 23, 55, 77 };
 	int n = 10;
 
-	for (int i = n - 1; i > 0; i--) {
+	for (int i = 0; i < n; i++) {
 
-		int maxIdx = 0;
+		for (int j = 0; j < n - 1 - i; j++) {
 
-		for (int j = 1; j <= i; j++) {
-			if (arr[maxIdx] < arr[j]) {
-				maxIdx = j;
-			}
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+
 		}
-
-		swap(arr[i], arr[maxIdx]);
+		
 	}
 
-	// Short
-	//for (int i = n - 1; i > 0; i--) {
-	//	swap(*max_element(arr, arr + i + 1), arr[i]);
-	//}
 
 	for (int i = 0; i < n; i++) {
 		cout << arr[i] << ' ';
