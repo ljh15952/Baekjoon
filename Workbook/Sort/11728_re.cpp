82615535ab897c46d0be2672026dd7cde71bd76f
중화이 <ljh15952@gmail.com>
2024-07-08 04:31:17 +0000

    Solve

--- Workbook/Sort/11728_re.cpp
+++ Workbook/Sort/11728_re.cpp
@@ -2,37 +2,49 @@
 
 using namespace std;
 
-int A[1000001];
-int B[1000001];
 
-int N, M;
+int A[1000005], B[1000005];
 
-int main(){
-	
+int arr[2000005];
+
+int main()
+{
+
 	ios::sync_with_stdio(0); cin.tie(0);
-	
+	int N, M;
 	cin >> N >> M;
-	
-	for(int i = 0; i < N; i++){
+
+	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
-	
-	for(int i = 0; i < M; i++){
-		cin >> B[i];	
+
+	for (int i = 0; i < M; i++) {
+		cin >> B[i];
 	}
-	
-	for(int i = 0; i < N; i++){
-		cout << A[i] << ' ';
+
+	int AIdx = 0, BIdx = 0;
+	int n = 0;
+
+	while (AIdx < N || BIdx < M) {
+		if (AIdx >= N) {
+			while (BIdx < M)
+				arr[n++] = B[BIdx++];
+		}
+		else if (BIdx >= M) {
+			while (AIdx < N)
+				arr[n++] = A[AIdx++];
+		}
+		else {
+			if (A[AIdx] < B[BIdx])
+				arr[n++] = A[AIdx++];
+			else
+				arr[n++] = B[BIdx++];
+		}
 	}
+
+	for (int i = 0; i < N + M; i++)
+		cout << arr[i] << ' ';
 	cout << '\n';
-	
-	for(int i = 0; i < M; i++){
-		cout << B[i] << ' ';
-	}
-	cout << '\n';
-	
-	
-	
-	
+
 	return 0;
 }
