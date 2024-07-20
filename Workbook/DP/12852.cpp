99c67cd8c6f5df0dffc36ac88aa3568c786d5ad4
중화이 <ljh15952@gmail.com>
2024-07-20 14:29:20 +0000

    Solving

---
+++ Workbook/DP/12852.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int x;
+
+int d[100005];
+int pre[100005];
+
+int main() {
+
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> x;
+
+	d[1] = 0;
+	pre[1] = 0;
+	
+
+	for (int i = 1; i <= x; i++) {
+
+		if (i % 3 == 0) {
+
+		}
+		else if (i % 2 == 0) {
+
+		}
+		else {
+
+		}
+
+	}
+
+
+
+	return 0;
+}
