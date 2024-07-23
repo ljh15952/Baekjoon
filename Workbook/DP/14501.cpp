404b3664a3dfae01d67265bcbfebefcda4ffeac4
중화이 <ljh15952@gmail.com>
2024-07-23 13:09:23 +0000

    Solve

--- Workbook/DP/14501.cpp
+++ Workbook/DP/14501.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int n;
+int t[16];
+int p[16];
+int d[16];
 
 int main() {
 
@@ -10,11 +13,20 @@ int main() {
 
 	cin >> n;
 
-	/*
-	
-	
-	*/
+	for (int i = 1; i <= n; i++) {
+		cin >> t[i] >> p[i];
+	}
 
+	for (int i = n; i >= 1; i--) {
+		if (i + t[i] <= n + 1) {
+			d[i] = max(d[i + t[i]] + p[i], d[i + 1]);
+		}
+		else {
+			d[i] = d[i + 1];
+		}
+	}
+
+	cout << *max_element(d, d + n + 1) << '\n';
 
 	return 0;
 }
