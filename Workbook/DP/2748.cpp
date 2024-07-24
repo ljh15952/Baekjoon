bcacfab0e003d7388d766d54a75fd81eca1a99b6
중화이 <ljh15952@gmail.com>
2024-07-24 13:07:33 +0000

    Solve

---
+++ Workbook/DP/2748.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+long long d[100];
+
+int main() {
+
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n;
+
+	d[0] = 0;
+	d[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 2] + d[i - 1];
+	}
+
+	cout << d[n] << '\n';
+
+	return 0;
+}
