4ff8a2a900819359139a499a27d888bfac2f0f1d
중화이 <ljh15952@gmail.com>
2024-07-24 12:55:33 +0000

    Solve

---
+++ Workbook/DP/15486.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int t[1500005];
+int p[1500005];
+int d[1500005];
+
+int main() {
+
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> t[i] >> p[i];
+	}
+
+	for (int i = n; i >= 1; i--) {
+		if (i + t[i] <= n + 1) {
+			d[i] = max(d[i + t[i]] + p[i], d[i + 1]);
+		}
+		else {
+			d[i] = d[i + 1];
+		}
+	}
+
+	cout << *max_element(d, d + n + 1) << '\n';
+
+	return 0;
+}
