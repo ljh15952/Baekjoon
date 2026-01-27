704329ba31a0312c9fd6a1177e673f68625395f0
LeeJeungHwa <ljh15952@gmail.com>
2026-01-27 16:48:59 +0900

    Update 4375.cpp

--- Add/4375.cpp
+++ Add/4375.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	while (cin >> n) {
+		int r = 0;
+		int len = 0;
+
+		while (1) {
+			r = (r * 10 + 1) % n;
+			len++;
+			if (r == 0) {
+				cout << len << "\n";
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
 
