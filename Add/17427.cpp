6708e7b8936d2e23d61bd47465872d2f7d6610e5
LeeJeungHwa <ljh15952@gmail.com>
2026-01-28 09:07:33 +0900

    Update 17427.cpp

--- Add/17427.cpp
+++ Add/17427.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long N;
+	cin >> N;
+
+	long long ans = 0;
+	for (long long d = 1; d <= N; d++) {
+		ans += d * (N / d);
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
