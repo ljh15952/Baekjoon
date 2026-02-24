51067ed1ab80bb27f63b42c0050c0b36e2e5d90b
LeeJeungHwa <ljh15952@gmail.com>
2026-02-24 10:56:13 +0900

    Update 1806.cpp

--- Add/1806.cpp
+++ Add/1806.cpp
@@ -1,42 +1,31 @@
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int N;
 long long M;
 long long arr[100005];
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) cin >> arr[i];
 
-	int l = 0, r = 0;
-	long long sum = 0;
-	int ans = 100001;
-	while (r <= N) {
+    int l = 0, r = 0;
+    long long sum = 0;
+    int ans = N + 1;
 
-		if (sum < M) {
-			sum += arr[r++];
-		}
-		else if (sum >= M) {
-			ans = min(ans, r - l);
-			sum -= arr[l++];
-		}
-	}
+    while (true) {
+        if (sum >= M) {
+            ans = min(ans, r - l);
+            sum -= arr[l++];
+        } else {
+            if (r == N) break;
+            sum += arr[r++];
+        }
+    }
 
-	if (ans == 100001)
-		ans = 0;
-	cout << ans << endl;
-
-	return 0;
+    cout << (ans == N + 1 ? 0 : ans) << "\n";
+    return 0;
 }
-
-/*
-*/
