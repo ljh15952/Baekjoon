7aecf1df7344f2c55f02a6e831b3166acda361ef
LeeJeungHwa <ljh15952@gmail.com>
2026-03-05 15:14:47 +0900

    Update 2110.cpp

--- Add/2110.cpp
+++ Add/2110.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+int N, C;
+long long arr[200005];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N >> C;
+	for (int i = 0;i < N;i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+
+	int l = 1;
+	int r = arr[N - 1] - arr[0];
+	int ans = 0;
+
+	while (l <= r) {
+		int mid = (l + r) / 2;
+
+		int cnt = 1;
+		int last = arr[0];
+
+		for (int i = 1; i < N; i++) {
+			if (arr[i] - last >= mid) {
+				cnt++;
+				last = arr[i];
+			}
+		}
+
+		if (cnt >= C) {
+			ans = mid;
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+	
+	cout << ans << "\n";
+
+	return 0;
+}
